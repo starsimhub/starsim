eb7927795723e07e1cb1ded4a019e7957f5a3aff
Romesh Abeysuriya <romesh.abey@gmail.com>
2023-05-30 15:21:29 +1000

    Going to try class-based models

--- coinfection_prototype.py
+++ coinfection_prototype.py
@@ -11,6 +11,7 @@
 import numpy as np
 import sciris as sc
 
+#### PEOPLE/POPULATIONS
 
 class State():
     # Simplified version of hpvsim.State()
@@ -30,41 +31,21 @@ class People():
 
     # Define states that every People instance has, regardless of which modules are enabled
     base_states = [
-        State('uid', int),
+        State('uid', int), # TODO: will we support removing agents? It could make indexing much more complicated...
         State('age', float),
         State('female', bool, False),
         State('dead', bool, False),
-        State('ti_dead', float, np.nan), # Time index for death by natural causes
+        State('ti_dead', float, np.nan), # Time index for death - defaults to natural causes but gets overwritten if they die of something first
     ]
 
     def __len__(self):
         return len(self.uid)
 
     def __getitem__(self, key):
-        ''' Allow people['attr'] instead of getattr(people, 'attr')
-            If the key is an integer, alias `people.person()` to return a `Person` instance
-        '''
-        if isinstance(key, int):
-            return self.person(key)
-        else:
-            return self.__getattribute__(key)
+        return self.__getattribute__(key)
 
     def __setitem__(self, key, value):
-        ''' Ditto '''
-        if self._lock and key not in self.__dict__: # pragma: no cover
-            errormsg = f'Key "{key}" is not a current attribute of people, and the people object is locked; see people.unlock()'
-            raise AttributeError(errormsg)
         return self.__setattr__(key, value)
-
-    def __setattr__(self, attr, value):
-        ''' Ditto '''
-        if hasattr(self, '_data') and attr in self._data:
-            # Prevent accidentally overwriting a view with an actual array - if this happens, the updated values will
-            # be lost the next time the arrays are resized
-            raise Exception('Cannot assign directly to a dynamic array view - must index into the view instead e.g. `people.uid[:]=`')
-        else:   # If not initialized, rely on the default behavior
-            obj_set(self, attr, value)
-        return
 
     @property
     def male(self):
@@ -74,9 +55,13 @@ class People():
     def n(self):
         return len(self)
 
+    @property
+    def indices(self):
+        return self.uid
+
     def __init__(self, n):
         # TODO - where is the right place to change how the initial population is generated?
-        for state in self.states:
+        for state in self.base_states:
             self.__setattr__(state.name, state.new(n))
         self.uid[:] = np.arange(n)
         self.age[:] = np.random.random(n)
@@ -85,6 +70,9 @@ class People():
 
     def add_module(self, module):
         # Initialize all of the states associated with a module
+        # This is implemented as People.add_module rather than
+        # Module.add_to_people(people) or similar because its primary
+        # role is to modify the
         self.__setattr__(module.name, sc.objdict)
         for state in module.states:
             self.__setattr__(state.name, state.new(self.n))
@@ -101,11 +89,13 @@ class People():
         for module in sim.modules:
             module.update_states_pre(sim)
 
-    def init_results(self, sim):
-        for module in sim.modules:
-            module.init_results(sim)
+    def initialize(self, sim):
+        sim.results['n_people'] = Result(None, 'n_people', sim.n, dtype=int)
+        sim.results['new_deaths'] = Result(None, 'new_deaths', sim.n, dtype=int)
 
     def update_results(self, sim):
+        sim.results['new_deaths'] = sim.people.ti_dead == sim.ti
+
         for module in sim.modules:
             module.update_results(sim)
 
@@ -113,8 +103,9 @@ class People():
         for module in sim.modules:
             module.finalize_results(sim)
 
-class Layer(sc.FlexDict):
-    def __init__(self, people: People):
+
+class Layer(sc.odict):
+    def __init__(self):
         self.p1 = np.array([], dtype=int)
         self.p2 = np.array([], dtype=int)
         self.beta = np.array([], dtype=float)
@@ -126,37 +117,28 @@ class Layer(sc.FlexDict):
     def update(self):
         pass
 
-
 # TODO - modify network to have long-term and casual partnerships with different transmission risk per contact
 class RandomDynamicSexualLayer(Layer):
     # Randomly pair males and females with variable relationship durations
     def __init__(self, people, mean_dur=5):
-        super().__init__(self, people)
+        super().__init__()
         self.mean_dur = mean_dur
         self.dur = np.array([], dtype=float)
-        self.update()
+        self.add_partnerships(people)
 
-    def update(self, people):
-        # Decrement time step
-        self.dur = self.dur - people.dt
-
-        # Remove any partnerships with a duration of 0
-        active = self.dur > 0
-        self.p1 = self.p1[active]
-        self.p2 = self.p2[active]
-        self.beta = self.beta[active]
-
+    def add_partnerships(self, people):
         # Find unpartnered males and females - could in principle check other contact layers too
         # by having the People object passed in here
-        available_m = np.setdiff(people.indices[people.male], self.members)
-        available_f = np.setdiff(people.indices[~people.male], self.members)
+
+        available_m = np.setdiff1d(people.indices[people.male], self.members)
+        available_f = np.setdiff1d(people.indices[~people.male], self.members)
 
         if len(available_m) <= len(available_f):
             p1 = available_m
-            p2 = np.random.sample(available_f, len(p1), replace=False)
+            p2 = np.random.choice(available_f, len(p1), replace=False)
         else:
             p2 = available_f
-            p1 = np.random.sample(available_m, len(p2), replace=False)
+            p1 = np.random.choice(available_m, len(p2), replace=False)
 
         beta = np.ones_like(p1)
         dur = np.random.randn(len(p1))*self.mean_dur
@@ -165,11 +147,25 @@ class RandomDynamicSexualLayer(Layer):
         self.beta = np.concatenate([self.beta, beta])
         self.dur = np.concatenate([self.dur, dur])
 
+    def update(self, people):
+        # First remove any relationships due to end
+        self.dur = self.dur - people.dt
+        active = self.dur > 0
+        self.p1 = self.p1[active]
+        self.p2 = self.p2[active]
+        self.beta = self.beta[active]
+
+        # Then add new relationships for unpartnered people
+        self.add_partnerships(people)
+
+
 
 def make_people(n):
     people = People(n)
     people.contacts['random'] = RandomDynamicSexualLayer(people)
+    return people
 
+##### DISEASE MODULES
 
 class Module():
     # Base module contains states/attributes that all modules have
@@ -183,16 +179,14 @@ class Module():
             else:
                 self.pars[k] = v
 
-    def init_people(self, people):
+    def initialize(self, sim):
         # Add this module to a People instance. This would always involve calling People.add_module
         # but subsequently modules could have their own logic for initializing the default values
-        people.add_module(self)
+        # and initializing any outputs that are required
+        sim.people.add_module(self)
 
     def update_states_pre(self, people):
         # Carry out any autonomous state changes at the start of the timestep
-        pass
-
-    def init_results(self):
         pass
 
     def update_results(self):
@@ -216,9 +210,9 @@ class HIV(Module):
     ]
 
     default_pars = {
-        'cd4_min' = 100,
-        'cd4_max' = 500,
-        'cd4_rate' = 2,
+        'cd4_min': 100,
+        'cd4_max': 500,
+        'cd4_rate': 2,
     }
 
 
@@ -232,7 +226,6 @@ class HIV(Module):
 
     def init_results( sim):
         pass
-
 
 
 class Result(np.lib.mixins.NDArrayOperatorsMixin):
@@ -256,7 +249,6 @@ class Result(np.lib.mixins.NDArrayOperatorsMixin):
         return self.values.__array_ufunc__(*args, **kwargs)
 
 
-
 class Gonorrhea(Module):
     states = [
         State('susceptible', bool, True),
@@ -273,21 +265,26 @@ class Gonorrhea(Module):
     def __init__(self, pars):
         super().__init__(pars)
 
-
     def update_states_pre(self, sim):
         # What if something in here should depend on another module?
         # I guess we could just check for it e.g., 'if HIV in sim.modules' or
         # 'if 'hiv' in sim.people' or something
-        sim.people.dead[sim.people.gonorrhea.ti_dead <= sim.ti] = True
+        gonorrhea_deaths = sim.people.gonorrhea.ti_dead <= sim.ti
+        sim.people.dead[gonorrhea_deaths] = True
+        sim.people.ti_dead[gonorrhea_deaths] = sim.ti
 
 
+    def initialize(self, sim):
+        super().initialize(sim)
 
-    def init_results(self, sim):
         # TODO - not a huge fan of having the result key duplicate the Result name
         sim.results[self.name,'n_susceptible'] = Result(self.name, 'n_susceptible', sim.n, dtype=int)
         sim.results[self.name,'n_infected'] = Result(self.name, 'n_infected', sim.n, dtype=int)
         sim.results[self.name,'prevalence'] = Result(self.name, 'prevalence', sim.n, dtype=float)
         sim.results[self.name,'new_infections'] = Result(self.name, 'n_infected', sim.n, dtype=int)
+
+        # Do any steps in this method depend on what other modules are going to be added? We can inspect
+        # them via sim.modules at this point
 
     def update_results(self, sim):
         # TODO sim.results.gonorrhea.susceptible or sim.results['gonorrhea.susceptible']?
@@ -295,6 +292,9 @@ class Gonorrhea(Module):
         sim.results[self.name,'n_infected'][sim.ti] = np.count_nonzero(sim.people.gonorrhea.infected)
         sim.results[self.name,'prevalence'][sim.ti] = sim.results.gonorrhea.infected[sim.ti] / sim.people.n
         sim.results[self.name,'new_infections'] = sim.people.gonorrhea.ti_infected == sim.ti
+
+
+
 
 class Analyzer():
     # TODO - what if the analyzer depends on a specific variable? How does the analyzer know which modules are required?
@@ -307,11 +307,14 @@ class Analyzer():
 
 
 
-class DefaultOutputs(Analyzer):
-    def initialize(self):
-        class Intervention():
+# class DefaultOutputs(Analyzer):
+#     def initialize(self):
 
-        class ART(Intervention):
+
+# class Intervention():
+
+
+# class ART(Intervention):
 
 
 
@@ -320,7 +323,7 @@ class DefaultOutputs(Analyzer):
 # A module is pre-loaded with parameters EXCLUDING person-generation pars
 
 class Sim():
-    def __init__(self, people, modules = None, pars=None, interventions=None, analyzers=None):
+    def __init__(self, people=None, modules=None, pars=None, interventions=None, analyzers=None):
         # TODO - clearer options for time units?
         #   ti - index self.ti = 0
         #    t - floating point year e.g., 2020.5
@@ -330,32 +333,32 @@ class Sim():
         self.ti = 0
         self.dt = 1 # could get this from pars of course
 
-        self.people = people
-        self.modules = sc.promotetolist(modules)
         self.pars = pars if pars is not None else sc.objdict()  # What pars are these? Are they by module? Are the parameters consumed when the module is created?
+        self.people = people if people is not None else make_people(self.pars.get('n',1000))
+        self.modules = sc.promotetolist(modules)
         self.interventions = sc.promotetolist(interventions)
         self.analyzers = sc.promotetolist(analyzers)
         self.results = {}
 
+    @property
+    def n(self):
+        return len(self.people)
 
     def initialize(self):
-        for module in self.modules:
-            module.init(self.people, self.pars) # Do this?
-
-    # @property
-    # def all_results(self):
-    #     for module, module_results in self.results.items():
-    #         for
-
-    def run(self):
 
         self.results = {}
 
+        self.people.initialize(sim)
+        for module in self.modules:
+            module.initialize(sim)
+
+
+    def run(self):
+
+        self.initialize()
+
         for i in range(self.pars.npts):
             self.step()
-
-
-
 
         res = self.people.update(t)
         self.results.append(res)
@@ -371,5 +374,12 @@ class Sim():
         self.people.update_results(sim)
 
 
+#######
+
+# TODO - should the module be stateful or stateless?
+
+people = make_people(1000)
+sim = Sim(people, [Gonorrhea])
+sim.run()
 
 
