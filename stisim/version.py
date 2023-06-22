e33007b5fb0c241d07016499faf0a21f8651f241
jamiecohen <jamie.alex.cohen@gmail.com>
2023-06-22 12:55:35 -0700

    version update

--- stisim/version.py
+++ stisim/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '0.0.0'
-__versiondate__ = '2023-05-31'
-__license__ = f'Starsim {__version__} ({__versiondate__}) — © 2023 by TBC'
+__versiondate__ = '2023-06-22'
+__license__ = f'STIsim {__version__} ({__versiondate__}) — © 2023 by TBC'
