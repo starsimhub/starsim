cc04fc4cee4b0dd30b01fd6b379b51b67fa0f098
Robyn Stuart <robyna.s@gmail.com>
2023-06-28 06:19:06 +1000

    add test

---
+++ tests/test_base.py
@@ -0,0 +1,110 @@
+"""
+Test objects from base.py
+"""
+
+# %% Imports and settings
+import sciris as sc
+import numpy as np
+import stisim as ss
+from collections import defaultdict
+import pytest
+import matplotlib.pyplot as plt
+
+
+# %% Define the tests
+
+def test_parsobj():
+    sc.heading('Testing parameters object')
+
+    pars1 = {'a': 1, 'b': 2}
+    parsobj = ss.ParsObj(pars1)
+
+    # Once created, you cannot directly add new keys to a parsobj, and a nonexistent key works like a dict
+    with pytest.raises(KeyError): parsobj['c'] = 3
+    with pytest.raises(KeyError): parsobj['c']
+
+    # Only a dict is allowed
+    with pytest.raises(TypeError):
+        pars2 = ['a', 'b']
+        ss.ParsObj(pars2)
+
+    return parsobj
+
+
+def test_people():
+    sc.heading('Testing base people object')
+
+    # Base people contains only the states defined in base.base_states
+    ppl = ss.BasePeople(100)  # BasePeople
+    del ppl
+
+    # Possible to initialize people with extra states
+    extra_states = ss.named_dict(
+        ss.State('debut', float),
+    )
+    ppl = ss.People(100, states=extra_states)  # BasePeople
+    ppl.add_module(ss.HIV())
+
+    return ppl
+
+
+def test_layers():
+
+    # Make completely abstract layers
+    n = 10_000
+    n_people = 1000
+    p1 = np.random.randint(n_people, size=n)
+    p2 = np.random.randint(n_people, size=n)
+    beta = np.ones(n)
+    layer1 = ss.Layer(p1=p1, p2=p2, beta=beta, label='rand')
+    layer2 = ss.Layer(dict(p1=p1, p2=p2, beta=beta), label='rand')  # Alternate method
+
+    # Make people with some extra states, then make a dynamic sexual layer and update it
+    states = ss.named_dict(
+        ss.State('debut', float),
+        ss.State('active', bool, True),
+    )
+    ppl = ss.People(100, states=states)  # BasePeople
+    layer3 = ss.DynamicSexualLayer()
+    layer3.initialize(ppl)  # Initialize with ti=0
+    layer3.update(ppl, ti=1, dt=1)  # Update with a timestep of 1 and
+
+    layer4 = ss.Maternal()
+    layer4.initialize(ppl)
+    layer4.add_pairs(mother_inds=[1, 2, 3], unborn_inds=[100, 101, 102], dur=[1, 1, 1])
+
+    return layer1, layer2, layer3, layer4
+
+
+def test_microsim():
+    sc.heading('Testing basic sim')
+
+    states = ss.named_dict(ss.State('active', bool, True))
+    ppl = ss.People(100, states=states)
+    ppl.contacts['random'] = ss.DynamicSexualLayer()
+
+    pars = defaultdict(dict)
+    pars['hiv']['beta'] = {'random': 0.3}
+
+    sim = ss.Sim(ppl, [ss.HIV, ss.Pregnancy], pars=pars)
+    sim.run()
+    plt.figure()
+    plt.plot(sim.tvec, sim.results.hiv.n_infected)
+    plt.title('HIV number of infections')
+    plt.show()
+    return sim
+
+
+# %% Run as a script
+if __name__ == '__main__':
+    # Start timing
+    T = sc.tic()
+
+    # Run tests
+    # parsobj = test_parsobj()
+    # ppl = test_people()
+    # layer1, layer2, layer3, layer4 = test_layers()
+    sim = test_microsim()
+
+    sc.toc(T)
+    print('Done.')
