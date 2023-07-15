24128c80b4703d3112615f6bd54b5d0794254170
Paula Sanz-Leon <pmsl.academic@gmail.com>
2023-07-15 17:40:15 +1000

    Test ParsObj

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -5,13 +5,48 @@ Test objects from parameters.py and default_parameters.py
 # %% Imports and settings
 import sciris as sc
 import numpy as np
-import stisim as ss
+import stisim
 from stisim import utils as ssu
 import matplotlib.pyplot as plt
-
+import pytest
 
 # %% Define the tests
 
+class TestParsObj:
+    @pytest.fixture
+    def pars_obj(self):
+        return stisim.ParsObj({'key1': 1, 'key2': 2})
+
+    def test_getitem_existing_key(self, pars_obj):
+        assert pars_obj['key1'] == 1
+
+    def test_getitem_nonexistent_key(self, pars_obj):
+        with pytest.raises(sc.KeyNotFoundError):
+            value = pars_obj['nonexistent']
+
+    def test_setitem_existing_key(self, pars_obj):
+        pars_obj['key1'] = 10
+        assert pars_obj['key1'] == 10
+
+    def test_setitem_nonexistent_key(self, pars_obj):
+        with pytest.raises(sc.KeyNotFoundError):
+            pars_obj['nonexistent'] = 100
+
+    def test_update_pars_valid_dict(self, pars_obj):
+        pars_obj.update_pars({'key3': 3})
+        assert pars_obj['key3'] == 3
+
+    def test_update_pars_invalid_dict(self, pars_obj):
+        with pytest.raises(TypeError):
+            pars_obj.update_pars('invalid_dict')
+
+    def test_update_pars_existing_key(self, pars_obj):
+        with pytest.raises(sc.KeyNotFoundError):
+            pars_obj.update_pars({'key1': 100}, create=False)
+
+    def test_update_pars_nonexistent_key(self, pars_obj):
+        with pytest.raises(sc.KeyNotFoundError):
+            pars_obj.update_pars({'nonexistent': 200}, create=False)
 
 
 def test_people():
@@ -59,14 +94,14 @@ def test_microsim():
 
 
 
-# %% Run as a script
-if __name__ == '__main__':
-    # Start timing
-    T = sc.tic()
-
-    # Run tests
-    ppl = test_people()
-    sim1 = test_microsim()
-
-    sc.toc(T)
-    print('Done.')
+# # %% Run as a script
+# if __name__ == '__main__':
+#     # Start timing
+#     T = sc.tic()
+#
+#     # Run tests
+#     ppl = test_people()
+#     sim1 = test_microsim()
+#
+#     sc.toc(T)
+#     print('Done.')
