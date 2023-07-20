4eac2e86d209895dfa04983516566d3774b67ff7
Paula Sanz-Leon <pmsl.academic@gmail.com>
2023-07-20 12:29:55 +1000

    Move default (legacy/current) pars to default_parameters.py

--- stisim/default_parameters.py
+++ stisim/default_parameters.py
@@ -6,12 +6,12 @@ import sciris as sc
 from .settings import options as sso  # For setting global options
 from .parameters import BaseParameter, ParameterSet
 
-__all__ = ['make_default_pars', 'make_default_parset', 'default_pars_dict', 'get_default_parameter', 'build_pars']
+__all__ = ['make_default_pars', 'build_default_pars', 'make_default_parset', 'default_pars_dict', 'get_default_parameter', 'build_pars']
 
 
-def make_default_pars(**kwargs):
+def build_default_pars(**kwargs):
     """
-    TODO: reenable used of kwargs if applicable
+    Build a dictionary with BaseParameter instances using default parameters from default_pars_dict
     """
     pars = build_pars(default_pars_dict())
     return pars
@@ -419,63 +419,64 @@ def build_pars(input_pars):
         pars[parameter_name] = BaseParameter(**parameter_data)
     return pars
 
-# def make_default_pars(**kwargs):
-#     """
-#     Create the parameters for the simulation. Typically, this function is used
-#     internally rather than called by the user; e.g. typical use would be to do
-#     sim = ss.Sim() and then inspect sim.pars, rather than calling this function
-#     directly.
-#
-#     #NOTE: current pars is acting as a more general inputs structure to the simulation, rather than
-#     as model parameters -- though it may be a matter of semantics what we categorise as parameters.
-#     Parameters are inputs but not all inputs are parameters?
-#
-#     Args:
-#         kwargs        (dict): any additional kwargs are interpreted as parameter names
-#     Returns:
-#         pars (dict): the parameters of the simulation
-#     """
-#     pars = sc.objdict()
-#
-#     # Population parameters
-#     pars['n_agents'] = 10e3  # Number of agents
-#     pars['total_pop'] = 10e3  # If defined, used for calculating the scale factor
-#     pars['pop_scale'] = None  # How much to scale the population
-#     pars['location'] = None  # What demographics to use - NOT CURRENTLY FUNCTIONAL
-#     pars['birth_rates'] = None  # Birth rates, loaded below
-#     pars['death_rates'] = None  # Death rates, loaded below
-#     pars['rel_birth'] = 1.0  # Birth rate scale factor
-#     pars['rel_death'] = 1.0  # Death rate scale factor
-#
-#     # Simulation parameters
-#     pars['start'] = 1995.  # Start of the simulation
-#     pars['end'] = None  # End of the simulation
-#     pars['n_years'] = 35  # Number of years to run, if end isn't specified. Note that this includes burn-in
-#     pars[
-#         'burnin'] = 25  # Number of years of burnin. NB, this is doesn't affect the start and end dates of the simulation, but it is possible remove these years from plots
-#     pars['dt'] = 1.0  # Timestep (in years)
-#     pars['dt_demog'] = 1.0  # Timestep for demographic updates (in years)
-#     pars['rand_seed'] = 1  # Random seed, if None, don't reset
-#     pars[
-#         'verbose'] = sso.verbose  # Whether or not to display information during the run -- options are 0 (silent), 0.1 (some; default), 1 (default), 2 (everything)
-#     pars['use_migration'] = True  # Whether to estimate migration rates to correct the total population size
-#
-#     # Events and interventions
-#     pars['connectors'] = sc.autolist()
-#     pars['interventions'] = sc.autolist()  # The interventions present in this simulation; populated by the user
-#     pars['analyzers'] = sc.autolist()  # The functions present in this simulation; populated by the user
-#     pars['timelimit'] = None  # Time limit for the simulation (seconds)
-#     pars['stopping_func'] = None  # A function to call to stop the sim partway through
-#
-#     # Network parameters, generally initialized after the population has been constructed
-#     pars['networks'] = sc.autolist()  # Network types and parameters
-#     pars['debut'] = dict(f=dict(dist='normal', par1=15.0, par2=2.0),
-#                          m=dict(dist='normal', par1=17.5, par2=2.0))
-#
-#     # Update with any supplied parameter values and generate things that need to be generated
-#     pars.update(kwargs)
-#
-#     return pars
+
+def make_default_pars(**kwargs):
+    """
+    Create the parameters for the simulation. Typically, this function is used
+    internally rather than called by the user; e.g. typical use would be to do
+    sim = ss.Sim() and then inspect sim.pars, rather than calling this function
+    directly.
+
+    #NOTE: current pars is acting as a more general inputs structure to the simulation, rather than
+    as model parameters -- though it may be a matter of semantics what we categorise as parameters.
+    Parameters are inputs but not all inputs are parameters?
+
+    Args:
+        kwargs        (dict): any additional kwargs are interpreted as parameter names
+    Returns:
+        pars (dict): the parameters of the simulation
+    """
+    pars = sc.objdict()
+
+    # Population parameters
+    pars['n_agents'] = 10e3  # Number of agents
+    pars['total_pop'] = 10e3  # If defined, used for calculating the scale factor
+    pars['pop_scale'] = None  # How much to scale the population
+    pars['location'] = None  # What demographics to use - NOT CURRENTLY FUNCTIONAL
+    pars['birth_rates'] = None  # Birth rates, loaded below
+    pars['death_rates'] = None  # Death rates, loaded below
+    pars['rel_birth'] = 1.0  # Birth rate scale factor
+    pars['rel_death'] = 1.0  # Death rate scale factor
+
+    # Simulation parameters
+    pars['start'] = 1995.  # Start of the simulation
+    pars['end'] = None  # End of the simulation
+    pars['n_years'] = 35  # Number of years to run, if end isn't specified. Note that this includes burn-in
+    pars[
+        'burnin'] = 25  # Number of years of burnin. NB, this is doesn't affect the start and end dates of the simulation, but it is possible remove these years from plots
+    pars['dt'] = 1.0  # Timestep (in years)
+    pars['dt_demog'] = 1.0  # Timestep for demographic updates (in years)
+    pars['rand_seed'] = 1  # Random seed, if None, don't reset
+    pars[
+        'verbose'] = sso.verbose  # Whether or not to display information during the run -- options are 0 (silent), 0.1 (some; default), 1 (default), 2 (everything)
+    pars['use_migration'] = True  # Whether to estimate migration rates to correct the total population size
+
+    # Events and interventions
+    pars['connectors'] = sc.autolist()
+    pars['interventions'] = sc.autolist()  # The interventions present in this simulation; populated by the user
+    pars['analyzers'] = sc.autolist()  # The functions present in this simulation; populated by the user
+    pars['timelimit'] = None  # Time limit for the simulation (seconds)
+    pars['stopping_func'] = None  # A function to call to stop the sim partway through
+
+    # Network parameters, generally initialized after the population has been constructed
+    pars['networks'] = sc.autolist()  # Network types and parameters
+    pars['debut'] = dict(f=dict(dist='normal', par1=15.0, par2=2.0),
+                         m=dict(dist='normal', par1=17.5, par2=2.0))
+
+    # Update with any supplied parameter values and generate things that need to be generated
+    pars.update(kwargs)
+
+    return pars
 
 # def get_births_deaths(location, verbose=1, by_sex=True, overall=False, die=True):
 #     """
