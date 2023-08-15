99cd3c05bd8ca8fe072432632f075ae52bd9719e
Robyn Stuart <robyna.s@gmail.com>
2023-08-15 12:36:07 +1000

    make test file

---
+++ tests/devtests/test_networks.py
@@ -0,0 +1,12 @@
+"""
+Experimenting with networks
+Key properties of networks:
+    - who participates? eligibility for selection vs uptake
+    -
+"""
+
+import stisim as ss
+
+
+class msm(ss.Network):
+    pass
