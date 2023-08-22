5c8f4db5556511c3f94edfcd68ae739bdbe04567
Robyn Stuart <robyna.s@gmail.com>
2023-08-23 07:03:48 +1000

    single network

--- tests/devtests/test_networks.py
+++ tests/devtests/test_networks.py
@@ -10,6 +10,7 @@ import sciris
 
 import stisim as ss
 from scipy.stats import norm
+import numpy as np
 
 ####################################
 # Adding states to people
@@ -21,24 +22,22 @@ ppl.age = norm.rvs(30, 10, 100)  # Give people non-zero ages
 ppl.add_network(msm)
 
 ####################################
-# How to represent MSM status
+# Experiments with representing MSM
 ####################################
 people = ss.People(100)
 # Old way:
 # Networks were stored in a dictionary of networks
 # people.contacts['msm']
 
-# New way:
+# Possible altenrative
 # people.routes.sexual is a single network that aggregates all subnetworks
 # We may also have
 # people.routes.injecting  # second type of network for STIsim
 # people.routes.airborne  # if modeling TB for e.g.
 
-#########################################################
-# Construction
-#########################################################
 import stisim.people as ssppl
 import stisim.utils as ssu
+
 
 def bs(name, prob, eligibility=None):
     """ Shorthand for defining a boolean state with a certain probability """
@@ -59,7 +58,7 @@ class Network:
                 ssppl.State('debut', float, 15),
                 ssppl.State('partners', int, 2),
                 ssppl.State('current_partners', int, 0),
-        ), states)
+            ), states)
 
 
 class msm(Network):
@@ -89,22 +88,93 @@ class SexualTransmission(Network):
 
     def find_participants(self, people):
         participants = ssu.true(self.states['participant'])
-
         for network in self.networks:
             network.states['participant'].get_eligibility(people)
 
 
-# Example
+# Examples
 unmarried = lambda ppl: ppl.networks.sexual_contacts.mf_marital.current_partners == 0
-married_msm = lambda ppl, rate: ppl.networks.sexual_contacts.mf_marital.current_partners == 0
 networks = [
     msm(pars=dict(part_rate=0.1)),
-    mf_casual(pars=dict(part_rate=0.3), eligibility=unmarried),  # Participation rates for these layers might be age/sex dependent
+    mf_casual(pars=dict(part_rate=0.3), eligibility=unmarried),
+    # Participation rates for these layers might be age/sex dependent
     mf_marital(pars=dict(part_rate=0.7))
 ]
 st = SexualTransmission(networks=networks)
 
 
+# This still hasn't solved the issue of how to define cross-network mixing
+
+#########################################################
+# Alternative method using only 1 sexual network and defining sexual orientation
+#########################################################
+class orientation:
+    def __init__(self, options=None, pop_shares=None, m_partner_prob=None):
+        self.options = options
+        self.pop_shares = pop_shares
+        self.m_partner_prob = m_partner_prob
+
+
+# Motivating examples:
+# 1. Sexual orientation patterns from Western countries
+western_prefs = orientation(
+    options=[0, 1, 2, 3, 4],  # Abridged Kinsey scale
+    m_partner_prob=[0.00, 0.005, 0.020, 0.100, 1.00],  # Probability of taking a male partner
+    pop_shares=[0.93, 0.040, 0.005, 0.005, 0.02]
+    # From https://en.wikipedia.org/wiki/Demographics_of_sexual_orientation
+)
+orientation1 = ssppl.State('orientation', int,
+                           distdict=dict(dist='choice', par1=western_prefs.m_partner_prob,
+                                         par2=western_prefs.pop_shares),
+                           eligibility='male')
+
+# 2. Sexual orientation with no MSM/MF mixing
+exclusive_prefs = orientation(
+    options=['mf', 'msm'],  # No mixing
+    m_partner_prob=[0.0, 1.0],  # Probability of taking a male partner
+    pop_shares=[0.95, 0.05]
+)
+
+# 3. Sexual orientation without open MSM
+closeted_prefs = orientation(
+    options=[0, 1, 2, 3],  # last category still only partners with MSM 50% of the time
+    m_partner_prob=[0.0, 0.005, 0.02, 0.50],  # Probability of taking a male partner
+    pop_shares=[0.9, 0.010, 0.03, 0.06]
+)
+
+# Now take the orientation distribution and pass it into the network.
+# Use a SexualNetwork class which has attributes like orientation (which other networks wouldn't have)
+
+
+class SexualNetwork(ss.Network):
+    def __init__(self, pars=None, states=None):
+        self.pars = ssu.omerge(dict(
+            part_rate=1,
+            orientation=closeted_prefs,
+        ), pars)
+        self.states = ssu.omerge(self.states, states)
+
+    def initialize(self, p):
+        """ Add states and do other initializations """
+        return
+
+    def add_pairs(self, p):
+        """
+        Partner-finding needs to account for sexual orientation.
+        Q, how can we incorporate age and other preferences into this?
+        """
+        male_uids = people.uid[people.male]
+
+        # Find males who have fewer than their desired number of partners
+        # Figure out who they want to partner with
+        # Questions:
+        #   - should there be assortativity between orientation, i.e. 6 more likely to pair with 6?
+        #   - how can we incorporate differences in duration/concurrency with age/orientation?
+
+
+#########################################################
+# Older ideas
+#########################################################
 # Networks store states
 # people.routes.sexual.msm.participant
 # people.routes.sexual.msm.active
@@ -117,23 +187,13 @@ st = SexualTransmission(networks=networks)
 # It has special methods for excluding double pairs
 #  sim.people.routes.sexual
 
-
-# # here, sim.people.msm is NOT a network
-#
-# sim.people.networks.sexual_contacts.msm  # and this?
-# sim.people.networks.sexual_contacts.participant
+# here, sim.people.msm is NOT a network
+# to find MSM, use
 # sim.people.networks.sexual_contacts.msm.participant
-#
 # sim.people.is_msm # refers to sim.people.networks.sexual_contacts.participant
 #
-# # Sexually active MSM
-# # ppl.networks['msm'].active(ppl)
-# # What about ppl.msm.active? Should ppl.msm be an objdict of states (as it is now), or the network itself?
-#
-# import stisim.utils as ssu
-# import stisim.people as ssppl
-#
-# # Make a school network
+# Ideas for other kinds of networks
+# A school network
 # def school_aged(people): return (people.age > 5) & (people.age < 18)
 # in_school = ssppl.State(
 #     'in_school',
@@ -159,7 +219,6 @@ st = SexualTransmission(networks=networks)
 #     fill_value=ssu.sample('choice', par1=[True,False], par2=[0.1,0.9]),
 #     eligibility='male'
 # )
-#
 # #
 # part_rates = {
 #     'age': [15,20,25,30,35,40,45,50,55,60,65,70,75,80,85],
