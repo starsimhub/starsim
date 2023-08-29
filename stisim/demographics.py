3f130539d7359d6c30c613b7befa007ac4868104
Robyn Stuart <robyna.s@gmail.com>
2023-08-29 12:36:20 +1000

    refactor pregnancy

--- stisim/demographics.py
+++ stisim/demographics.py
@@ -26,6 +26,8 @@ class Pregnancy(ss.Module):
             ss.State('ti_dead', float, np.nan),  # Maternal mortality
             self.states,
         )
+        for state in self.states.values():
+            self.__setattr__(state.name, state)
 
         self.pars = ss.omerge({
             'dur_pregnancy': 0.75,  # Make this a distribution?
@@ -36,7 +38,6 @@ class Pregnancy(ss.Module):
         }, self.pars)
 
         return
-
 
     def initialize(self, sim):
         super().initialize(sim)
@@ -51,11 +52,10 @@ class Pregnancy(ss.Module):
         Still unclear whether this logic should live in the pregnancy module, the
         individual disease modules, the connectors, or the sim.
         """
-        self.results['pregnancies'] = ss.Result('pregnancies', self.name, sim.npts, dtype=int)
-        self.results['births']      = ss.Result('births', self.name, sim.npts, dtype=int)
+        self.results += ss.Result('pregnancies', self.name, sim.npts, dtype=int)
+        self.results += ss.Result('births', self.name, sim.npts, dtype=int)
         return
-    
-    
+
     def update(self, sim):
         """
         Perform all updates
@@ -65,33 +65,31 @@ class Pregnancy(ss.Module):
         self.update_results(sim)
         return
 
-
     def update_states(self, sim):
         """
         Update states
         """
 
         # Check for new deliveries
-        deliveries = sim.people[self.name].pregnant & (sim.people[self.name].ti_delivery <= sim.ti)
-        sim.people[self.name].pregnant[deliveries] = False
-        sim.people[self.name].postpartum[deliveries] = True
-        sim.people[self.name].susceptible[deliveries] = False
-        sim.people[self.name].ti_delivery[deliveries] = sim.ti
+        deliveries = self.pregnant & (self.ti_delivery <= sim.ti)
+        self.pregnant[deliveries] = False
+        self.postpartum[deliveries] = True
+        self.susceptible[deliveries] = False
+        self.ti_delivery[deliveries] = sim.ti
 
         # Check for new women emerging from post-partum
-        postpartum = ~sim.people[self.name].pregnant & (sim.people[self.name].ti_postpartum <= sim.ti)
-        sim.people[self.name].postpartum[postpartum] = False
-        sim.people[self.name].susceptible[postpartum] = True
-        sim.people[self.name].ti_postpartum[postpartum] = sim.ti
+        postpartum = ~self.pregnant & (self.ti_postpartum <= sim.ti)
+        self.postpartum[postpartum] = False
+        self.susceptible[postpartum] = True
+        self.ti_postpartum[postpartum] = sim.ti
 
         # Maternal deaths
-        maternal_deaths = ss.true(sim.people[self.name].ti_dead <= sim.ti)
+        maternal_deaths = ss.true(self.ti_dead <= sim.ti)
         if len(maternal_deaths):
             sim.people.alive[maternal_deaths] = False
             sim.people.ti_dead[maternal_deaths] = sim.ti
 
         return
-
 
     def make_pregnancies(self, sim):
         """
@@ -104,7 +102,7 @@ class Pregnancy(ss.Module):
         # If incidence of pregnancy is non-zero, make some cases
         # Think about how to deal with age/time-varying fertility
         if self.pars.inci > 0:
-            denom_conds = ppl.female & ppl.active & ppl[self.name].susceptible
+            denom_conds = ppl.female & ppl.active & self.susceptible
             inds_to_choose_from = ss.true(denom_conds)
             uids = ss.binomial_filter(self.pars.inci, inds_to_choose_from)
 
@@ -129,7 +127,6 @@ class Pregnancy(ss.Module):
 
         return
 
-
     def set_prognoses(self, sim, uids):
         """
         Make pregnancies
@@ -137,26 +134,24 @@ class Pregnancy(ss.Module):
         Also reconciliation with birth rates
         Q, is this also a good place to check for other conditions and set prognoses for the fetus?
         """
-        pars = sim.pars[self.name]
 
         # Change states for the newly pregnant woman
-        sim.people[self.name].susceptible[uids] = False
-        sim.people[self.name].pregnant[uids] = True
-        sim.people[self.name].ti_pregnant[uids] = sim.ti
+        self.susceptible[uids] = False
+        self.pregnant[uids] = True
+        self.ti_pregnant[uids] = sim.ti
 
         # Outcomes for pregnancies
-        dur = np.full(len(uids), sim.ti + pars['dur_pregnancy'] / sim.dt)
-        dead = np.random.random(len(uids)) < sim.pars[self.name].p_death
-        sim.people[self.name].ti_delivery[uids] = dur  # Currently assumes maternal deaths still result in a live baby
-        dur_post_partum = np.full(len(uids), dur + pars['dur_postpartum'] / sim.dt)
-        sim.people[self.name].ti_postpartum[uids] = dur_post_partum
+        dur = np.full(len(uids), sim.ti + self.pars.dur_pregnancy / sim.dt)
+        dead = np.random.random(len(uids)) < self.pars.p_death
+        self.ti_delivery[uids] = dur  # Currently assumes maternal deaths still result in a live baby
+        dur_post_partum = np.full(len(uids), dur + self.pars.dur_postpartum / sim.dt)
+        self.ti_postpartum[uids] = dur_post_partum
 
         if np.count_nonzero(dead):
-            sim.people[self.name].ti_dead[uids[dead]] = dur[dead]
+            self.ti_dead[uids[dead]] = dur[dead]
         return
 
     def update_results(self, sim):
-        mppl = sim.people[self.name] # TODO: refactor with states owning their own data
-        self.results['pregnancies'][sim.ti] = np.count_nonzero(mppl.ti_pregnant == sim.ti)
-        self.results['births'][sim.ti] = np.count_nonzero(mppl.ti_delivery == sim.ti)
+        self.results['pregnancies'][sim.ti] = np.count_nonzero(self.ti_pregnant == sim.ti)
+        self.results['births'][sim.ti] = np.count_nonzero(self.ti_delivery == sim.ti)
         return
