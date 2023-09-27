345753841618a0cf59b4e4f10843593615be1a18
Robyn Stuart <robyna.s@gmail.com>
2023-09-27 14:34:19 +1000

    move file

--- stisim/connectors.py
+++ stisim/connectors.py
@@ -13,11 +13,6 @@ class Connector(ss.Module):
         return
 
 
-class Connectors(ss.ndict):
-    def __init__(self, *args, type=Connector, **kwargs):
-        return super().__init__(self, *args, type=type, **kwargs)
-
-
 #%% Individual connectors
 
 class simple_hiv_ng(Connector):
