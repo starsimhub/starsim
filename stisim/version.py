2daa9c40d416d1c96a3636846e86a492604b7ff3
Daniel Klein <daniel.klein@gatesfoundation.org>
2023-10-28 14:30:38 -0700

    Bumping version

--- stisim/version.py
+++ stisim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.0.8'
-__versiondate__ = '2023-10-04'
+__version__ = '0.0.9'
+__versiondate__ = '2023-10-28'
 __license__ = f'STIsim {__version__} ({__versiondate__}) — © 2023 by IDM'
