b116f662fe19628896174585209c14ddf47284af
Daniel Klein <daniel.klein@gatesfoundation.org>
2024-01-03 16:08:49 -0800

    Adding a new ScipyHistogram class to enable SciPy's `rv_histogram` to use common random number slotting.
    Tests pass locally, but `test_syphilis.py` is very slow. Might be due to handling of vital dynamics?

--- stisim/distributions.py
+++ stisim/distributions.py
@@ -9,10 +9,10 @@ from stisim.utils import INT_NAN
 from stisim.random import SingleRNG, MultiRNG
 from stisim import options, int_
 from copy import deepcopy
-
-__all__ = ['ScipyDistribution']
-
 from scipy.stats._discrete_distns import bernoulli_gen
+from scipy.stats import rv_histogram
+
+__all__ = ['ScipyDistribution', 'ScipyHistogram']
 
 
 class ScipyDistribution():
@@ -154,8 +154,6 @@ class ScipyDistribution():
                     return vals[size]
                 else:
                     return vals[slots] # slots defined above
-            
-
 
         self.rng = self.set_rng(rng, gen)
         self.gen = starsim_gen(name=gen.dist.name, seed=self.rng)(**gen.kwds)
@@ -218,6 +216,91 @@ class ScipyDistribution():
 
     def filter(self, size, **kwargs):
         return size[self.gen.rvs(size, **kwargs)]
+
+
+class ScipyHistogram(rv_histogram):
+    def __init__(self, *args, rng=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.random_state = self.set_rng(rng)
+        return
+
+    def initialize(self, sim, context):
+        # Note: context not used here, but maintained for consistency with ScipyDistribution
+        # Passing sim and context here allow callables to receive "self" and sim pointers
+        if isinstance(self.random_state, (SingleRNG, MultiRNG)):
+            self.random_state.initialize(sim.rng_container, sim.people.slot)
+        return
+
+    def set_rng(self, rng):
+        # Handle random generators
+        ret = self.random_state # Default
+        if options.multirng and rng and (self.random_state == np.random.mtrand._rand):
+            # MultiRNG, rng not none, and the current "random_state" is the
+            # numpy global singleton... so let's override
+            if isinstance(rng, str):
+                ret = MultiRNG(rng) # Crate a new generator with the user-provided string
+            elif isinstance(rng, np.random.Generator):
+                ret = rng
+            else:
+                raise Exception(f'The rng must be a string or a np.random.Generator instead of {type(rng)}')
+        return ret
+
+    def rvs(self, *args, **kwargs):
+        """
+        Return a specified number of samples from the distribution
+        """
+
+        size = kwargs['size']
+        slots = None
+        # Work out how many samples to draw. If sampling by UID, this depends on the slots assigned to agents.
+        if np.isscalar(size):
+            if not isinstance(size, (int, np.int64, int_)):
+                raise Exception('Input "size" must be an integer')
+            if size < 0:
+                raise Exception('Input "size" cannot be negative')
+            elif size == 0:
+                return np.array([], dtype=int) # int dtype allows use as index, e.g. when filtering
+            else:
+                n_samples = size
+        elif len(size) == 0:
+            return np.array([], dtype=int)
+        elif size.dtype == bool:
+            n_samples = len(size) if options.multirng else size.sum()
+        elif size.dtype in [int, np.int64, int_]:
+            if not options.multirng:
+                n_samples = len(size)
+            else:
+                v = size.__array__() # TODO - check if this works without calling __array__()?
+                try:
+                    slots = self.random_state.slots[v].__array__()
+                    max_slot = slots.max()
+                except AttributeError as e:
+                    if not isinstance(self.random_state, MultiRNG):
+                        raise Exception('With options.multirng and passing agent UIDs to a distribution, the random_state of the distribution must be a MultiRNG.')
+                    else:
+                        if not self.random_state.initialized:
+                            raise Exception('The MultiRNG instance must be initialized before use.')
+                    raise e
+
+                if max_slot == INT_NAN:
+                    raise Exception('Attempted to sample from an INT_NAN slot')
+                n_samples = max_slot + 1
+        else:
+            raise Exception("Unrecognized input type")
+
+        kwargs['size'] = n_samples
+        vals = super().rvs(*args, **kwargs)
+        
+        # _select:
+        if not options.multirng:
+            return vals
+
+        if np.isscalar(size):
+            return vals
+        elif size.dtype == bool:
+            return vals[size]
+        else:
+            return vals[slots] # slots defined above
 
 '''
 from scipy.stats import bernoulli
