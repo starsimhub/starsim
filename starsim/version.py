d625e4ff503a906d43d653711ae6a8a518eb126a
Paula Sanz-Leon <pmsl.academic@gmail.com>
2024-02-09 15:33:48 +1000

    Fix #230: update name to starsim and version date, but not version

--- starsim/version.py
+++ starsim/version.py
@@ -5,5 +5,5 @@ Version and license information.
 __all__ = ['__version__', '__versiondate__', '__license__']
 
 __version__ = '0.1.8'
-__versiondate__ = '2024-01-30'
-__license__ = f'STIsim {__version__} ({__versiondate__}) — © 2024 by IDM'
+__versiondate__ = '2024-02-09'
+__license__ = f'starsim {__version__} ({__versiondate__}) — © 2024 by IDM'
