3ea7e4a637d390675efeb4635bf3e893994ccc00
Cliff Kerr <questionably@gmail.com>
2024-02-12 14:34:37 -0500

    working on test example

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -3,17 +3,34 @@ Test simple APIs
 """
 
 # %% Imports and settings
-import numpy as np
 import starsim as ss
-import pandas as pd
-import matplotlib.pyplot as plt
-import scipy.stats as sps
 
 
 def test_simple():
 
     pars = dict(
-            n_agents = 10_000,
+        n_agents = 10_000,
+        networks = dict(
+            name= 'random',
+            n_contacts = 4,
+        ),
+        diseases = dict(
+            name = 'sir',
+            dur_inf = 10,
+            beta=0.1
+        )
+    )
+
+    sim = ss.Sim(pars)
+    sim.run()
+    # sim.plot_states()
+    return sim
+
+
+def test_simple_v2():
+
+    pars = dict(
+        n_agents = 10_000,
         networks = dict(
             name= 'random',
             n_contacts = 4,
@@ -33,6 +50,7 @@ def test_simple():
 
 if __name__ == '__main__':
 
-    sim = test_simple()
+    sim1 = test_simple()
+    sim1 = test_simple_v2()
 
 
