543ec3e0359ed665d45a68a2774d5d403ec73262
Cliff Kerr <questionably@gmail.com>
2024-02-12 15:12:08 -0500

    wrote new api tests

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -5,52 +5,53 @@ Test simple APIs
 # %% Imports and settings
 import starsim as ss
 
+n_agents = 2_000
 
-def test_simple():
 
-    pars = dict(
-        n_agents = 10_000,
-        networks = dict(
-            name= 'random',
-            n_contacts = 4,
-        ),
-        diseases = dict(
-            name = 'sir',
-            dur_inf = 10,
-            beta=0.1
-        )
-    )
-
-    sim = ss.Sim(pars)
-    sim.run()
-    # sim.plot_states()
+def test_default():
+    """ Create, run, and plot a sim with default settings """
+    sim = ss.Sim(n_agents=n_agents).run()
+    sim.plot()
     return sim
 
 
-def test_simple_v2():
+def test_simple():
+    """ Create, run, and plot a sim with specified parameters """
 
     pars = dict(
-        n_agents = 10_000,
+        n_agents = n_agents,
         networks = dict(
-            name= 'random',
+            name = 'random',
             n_contacts = 4,
         ),
         diseases = dict(
             name = 'sir',
             dur_inf = 10,
-            beta=0.1
+            beta = 0.1,
         )
     )
 
     sim = ss.Sim(pars)
     sim.run()
-    # sim.plot_states()
-    return  sim
+    sim.plot()
+    return sim
+
+
+def test_components():
+    """ Create, run, and plot a sim by assembling components """
+    people = ss.People(n_agents=n_agents)
+    network = ss.networks.random(n_contacts=4)
+    sir = ss.SIR(dur_inf=10, beta=0.1)
+    sim = ss.Sim(diseases=sir, people=people, networks=network)
+    sim.run()
+    sim.plot()
+    return sim
 
 
 if __name__ == '__main__':
 
-    sim1 = test_simple()
-    sim1 = test_simple_v2()
+    sim1 = test_default()
+    sim2 = test_simple()
+    sim3 = test_components()
 
 
