db16281b2957d470794e17b9536da9b7ae9f6791
Cliff Kerr <questionably@gmail.com>
2024-02-15 01:50:55 -0500

    done with sir

--- starsim/diseases/sir.py
+++ starsim/diseases/sir.py
@@ -7,7 +7,6 @@ import starsim as ss
 
 __all__ = ['SIR']
 
-
 class SIR(ss.Infection):
     """
     Example SIR model
@@ -18,28 +17,29 @@ class SIR(ss.Infection):
     """
 
     def __init__(self, pars=None, par_dists=None, *args, **kwargs):
-        pars = ss.omerge({
-            'dur_inf': 1,
-            'init_prev': 0.1,
-            'p_death': 0.2,
-            'beta': None,
-        }, pars)
+        pars = ss.omergeleft(pars,
+            dur_inf = 1,
+            init_prev = 0.1,
+            p_death = 0.2,
+            beta = None,
+        )
 
-        par_dists = ss.omerge({
-            'dur_inf': ss.lognorm,
-            'init_prev': ss.bernoulli,
-            'p_death': ss.bernoulli,
-        }, par_dists)
+        par_dists = ss.omergeleft(par_dists,
+            dur_inf   = ss.lognorm,
+            init_prev = ss.bernoulli,
+            p_death   = ss.bernoulli,
+        )
 
         super().__init__(pars=pars, par_dists=par_dists, *args, **kwargs)
 
-        self.susceptible = ss.State('susceptible', bool, True)
-        self.infected = ss.State('infected', bool, False)
-        self.recovered = ss.State('recovered', bool, False)
-        self.ti_infected = ss.State('ti_infected', float, np.nan)
-        self.ti_recovered = ss.State('ti_recovered', float, np.nan)
-        self.ti_dead = ss.State('ti_dead', float, np.nan)
-
+        self.add_states(
+            ss.State('susceptible', bool, True),
+            ss.State('infected', bool, False),
+            ss.State('recovered', bool, False),
+            ss.State('ti_infected', float, np.nan),
+            ss.State('ti_recovered', float, np.nan),
+            ss.State('ti_dead', float, np.nan),
+        )
         return
 
     def update_pre(self, sim):
@@ -52,7 +52,6 @@ class SIR(ss.Infection):
         deaths = ss.true(self.ti_dead <= sim.year)
         if len(deaths):
             sim.people.request_death(deaths)
-
         return
 
     def update_death(self, sim, uids):
