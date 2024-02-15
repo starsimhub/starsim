1be3c307c74d075a3c94f727d02a16e1bca3ae11
Robyn Stuart <robyna.s@gmail.com>
2024-02-15 14:09:00 +1100

    fit for purpose

--- starsim/diseases/__init__.py
+++ starsim/diseases/__init__.py
@@ -1,8 +1,9 @@
 from .sir           import *
 from .ncd           import *
 
-# Specific diseases
+# Specific diseases listed alphabetically
 from .cholera       import *
+from .ebola         import *
 from .gonorrhea     import *
 from .hiv           import *
 from .measles       import *
