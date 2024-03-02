96afb337e1943517c7ad07a4f4c5b09aa682c697
Daniel Klein <daniel.klein@gatesfoundation.org>
2024-03-01 23:46:53 -0800

    Option string formatting.

--- starsim/settings.py
+++ starsim/settings.py
@@ -65,14 +65,7 @@ class Options(sc.objdict):
         optdesc = sc.objdict()  # Help for the options
         options = sc.objdict()  # The options
 
-        optdesc.rng = 'Set how random numbers are handled in Starsim with three options:\
-            "centralized" uses the centralized numpy random number generator for all distributions\
-            "single" uses a separate (default) random number generator for each distribution\
-            "multi" uses a separate (MultiRNG) random number generator for each distribution.\
-            In comparing two simulations, the "single" option may be slightly better than the\
-            "centralized" option without any real disadvantages. Only "multi" can achieve full\
-            common random number (CRN) coherence, but is more computationally expensive and only\
-            produces CRN results when using CRN-safe code.'
+        optdesc.rng = 'Set how random numbers are handled in Starsim with three options: 1) "centralized" uses the centralized numpy random number generator for all distributions. 2) "single" uses a separate (SingleRNG) random number generator for each distribution. 3) "multi" uses a separate (MultiRNG) random number generator for each distribution. In comparing two simulations, the "single" option may be slightly better than the "centralized" option without any real disadvantages. Only "multi" can achieve full common random number (CRN) coherence, but is more computationally expensive and only produces CRN results when using CRN-safe code. The default value is "single".'
         options.rng = ['centralized', 'single', 'multi'][1]
 
         optdesc.verbose = 'Set default level of verbosity (i.e. logging detail): e.g., 0.1 is an update every 10 simulated timesteps.'
