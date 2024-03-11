1823f9116297e57905d127f0c96b2541bea3995d
Daniel Klein <daniel.klein@gatesfoundation.org>
2024-03-11 11:51:48 -0700

    Refactoring so that MultiRNG is a subclass of SingleRNG, following the suggestion from @cliffckerr.

--- starsim/random.py
+++ starsim/random.py
@@ -105,143 +105,6 @@ def RNG(*args, **kwargs):
     # Centralized
     return np.random.mtrand._rand
 
-
-class MultiRNG(np.random.Generator):
-    """
-    Class for tracking one random number generators associated with one decision per timestep.
-
-    The main use case is to sample random numbers from various distributions
-    that are specific to each agent (per decision and timestep) so as to enable
-    variance reduction between simulations through the use of common random
-    numbers. For example, the user might create a random number generator called
-    rng and ultimately ask for randomly distributed random numbers for agents
-    with UIDs 1 and 4:
-
-    >>> import starsim as ss
-    >>> import numpy as np
-    >>> rng = ss.MultiRNG('Test') # The hashed name determines the seed offset.
-    >>> rng.initialize(container=None, slots=5) # In practice, slots will be sim.people.slots. When scalar (for testing), an np.arange will be used.
-    >>> uids = np.array([1,4])
-    >>> rng.random(uids)
-    array([0.88110549, 0.86915719])
-
-    In theory, what this is doing is drawing 5 random numbers and returning the
-    draws at positions 1 and 4.
-
-    In practice, using UIDs as "slots" (the indices into the larger draw) falls
-    apart when new agents are born.  The issue is that one simulation might have
-    more births than another, so an agent born in one simulation may not
-    get the same UID as that same agent in a comparison simulation.
-    
-    The solution applied here is for each agent to have a property called "slot"
-    that is precisely the index used when selecting from an array of random
-    numbers.  When new agents are born, the mother uses her UID to sample a
-    random integer for the newborn that is used as the "slot".  With this
-    approach, newborns will be identical between two different simulations,
-    unless an intervention mechanistically drove a change.
-
-    The slot-based approach is not without challenges.
-    * Two newborn agents may received the same "slot," and thus will receive the
-      same random draws.
-    * The chance of overlapping slots can be reduced by
-      allowing mothers to choose from a larger number of possible slots (say up
-      to one-million). However, as slots are used as indices, the number of
-      random variables drawn for each query must number the maximum slot. So if
-      one agent has a slot of 1M, then 1M random numbers will be drawn,
-      consuming more time than would be necessary if the maximum slot was
-      smaller.
-    * The maximum slot is now determined by a new configure parameter named
-      "slot_scale". A value of 5 will mean that new agents will be assigned
-      slots between 1*N and 5*N, where N is sim.pars['n_agents'].
-    """
-    
-    def __init__(self, name, seed_offset=None, **kwargs):
-        """
-        Create a random number generator
-
-        seed_offset will be automatically assigned (based on hashing the name) if None
-        
-        name: a name for this random number generator, like "coin_flip"
-        """
-
-        self.name = name
-        self.kwargs = kwargs
-
-        if seed_offset is None:
-            # Obtain the seed offset by hashing the class name. Don't use python's hash because it is randomized.
-            self.seed_offset = int(hashlib.sha256(self.name.encode('utf-8')).hexdigest(), 16) % 10**8
-        else:
-            # Use user-provided seed_offset (unlikely)
-            self.seed_offset = seed_offset
-
-        self.seed = None # Will be determined once added to the RNG Container
-        self.initialized = False
-        self.ready = True
-        return
-
-    def initialize(self, container, slots):
-        if self.initialized:
-            return
-
-        if container is not None:
-            self.seed = container.add(self) # base_seed + seed_offset
-        else:
-            # Enable use of MultiRNG without a container
-            self.seed = self.seed_offset
-
-        if isinstance(slots, int):
-            # Handle edge case in which the user wants n sequential slots, as used in testing.
-            self.slots = np.arange(slots)
-        else:
-            self.slots = slots # E.g. sim.people.slots (instead of using uid as the slots directly)
-
-        if 'bit_generator' not in self.kwargs:
-            self.kwargs['bit_generator'] = np.random.PCG64DXSM(seed=self.seed)
-        super().__init__(**self.kwargs)
-
-        self._init_state = self.bit_generator.state # Store the initial state
-
-        self.initialized = True
-        self.ready = True
-        return
-
-    def reset(self):
-        """ Restore initial state """
-        self.bit_generator.state = self._init_state
-        self.ready = True
-        return
-
-    def step(self, ti):
-        """ Advance to time ti step by jumping """
-        
-        # First reset back to the initial state
-        self.reset()
-
-        # Now take ti jumps
-        # jumped returns a new bit_generator, use directly instead of setting state?
-        self.bit_generator.state = self.bit_generator.jumped(jumps=ti).state
-        return
-
-    def __deepcopy__(self, memo):
-        cls = self.__class__
-        result = cls.__new__(cls)
-        memo[id(self)] = result
-        for k, v in self.__dict__.items():
-            setattr(result, k, deepcopy(v, memo))
-
-        #'bit_generator' kwarg has changed
-        super(MultiRNG, result).__init__(**result.kwargs)
-
-        # and so has the _init_state
-        result._init_state = result.bit_generator.state # Store the initial state
-
-        self.seed = None # Will be determined once added to the RNG Container
-        result.initialized = True
-        result.ready = True
-
-        return result
-
-
 class SingleRNG(np.random.Generator):
     """
     Enable a single random number generator stream per distribution without the complexity of MultiRNG.
@@ -311,3 +174,105 @@ class SingleRNG(np.random.Generator):
         result.ready = True
 
         return result
+
+class MultiRNG(SingleRNG):
+    """
+    Class for tracking one random number generators associated with one decision per timestep.
+
+    The main use case is to sample random numbers from various distributions
+    that are specific to each agent (per decision and timestep) so as to enable
+    variance reduction between simulations through the use of common random
+    numbers. For example, the user might create a random number generator called
+    rng and ultimately ask for randomly distributed random numbers for agents
+    with UIDs 1 and 4:
+
+    >>> import starsim as ss
+    >>> import numpy as np
+    >>> rng = ss.MultiRNG('Test') # The hashed name determines the seed offset.
+    >>> rng.initialize(container=None, slots=5) # In practice, slots will be sim.people.slots. When scalar (for testing), an np.arange will be used.
+    >>> uids = np.array([1,4])
+    >>> rng.random(uids)
+    array([0.88110549, 0.86915719])
+
+    In theory, what this is doing is drawing 5 random numbers and returning the
+    draws at positions 1 and 4.
+
+    In practice, using UIDs as "slots" (the indices into the larger draw) falls
+    apart when new agents are born.  The issue is that one simulation might have
+    more births than another, so an agent born in one simulation may not
+    get the same UID as that same agent in a comparison simulation.
+    
+    The solution applied here is for each agent to have a property called "slot"
+    that is precisely the index used when selecting from an array of random
+    numbers.  When new agents are born, the mother uses her UID to sample a
+    random integer for the newborn that is used as the "slot".  With this
+    approach, newborns will be identical between two different simulations,
+    unless an intervention mechanistically drove a change.
+
+    The slot-based approach is not without challenges.
+    * Two newborn agents may received the same "slot," and thus will receive the
+      same random draws.
+    * The chance of overlapping slots can be reduced by
+      allowing mothers to choose from a larger number of possible slots (say up
+      to one-million). However, as slots are used as indices, the number of
+      random variables drawn for each query must number the maximum slot. So if
+      one agent has a slot of 1M, then 1M random numbers will be drawn,
+      consuming more time than would be necessary if the maximum slot was
+      smaller.
+    * The maximum slot is now determined by a new configure parameter named
+      "slot_scale". A value of 5 will mean that new agents will be assigned
+      slots between 1*N and 5*N, where N is sim.pars['n_agents'].
+    """
+    
+    def __init__(self, name, seed_offset=None, **kwargs):
+        """
+        Create a random number generator
+
+        seed_offset will be automatically assigned (based on hashing the name) if None
+        
+        name: a name for this random number generator, like "coin_flip"
+        """
+
+        super().__init__(name, seed_offset, **kwargs)
+
+        if seed_offset is None:
+            # Obtain the seed offset by hashing the class name. Don't use python's hash because it is randomized.
+            self.seed_offset = int(hashlib.sha256(self.name.encode('utf-8')).hexdigest(), 16) % 10**8
+        else:
+            # Use user-provided seed_offset (unlikely)
+            self.seed_offset = seed_offset
+
+        self.seed = None # Will be determined once added to the RNG Container
+        self.ready = True
+        return
+
+    def initialize(self, container, slots):
+        super().initialize(container, slots)
+
+        if isinstance(slots, int):
+            # Handle edge case in which the user wants n sequential slots, as used in testing.
+            self.slots = np.arange(slots)
+        else:
+            self.slots = slots # E.g. sim.people.slots (instead of using uid as the slots directly)
+
+        self._init_state = self.bit_generator.state # Store the initial state
+
+        self.ready = True
+        return
+
+    def reset(self):
+        """ Restore initial state """
+        self.bit_generator.state = self._init_state
+        self.ready = True
+        return
+
+    def step(self, ti):
+        """ Advance to time ti step by jumping """
+        
+        # First reset back to the initial state
+        self.reset()
+
+        # Now take ti jumps
+        # jumped returns a new bit_generator, use directly instead of setting state?
+        self.bit_generator.state = self.bit_generator.jumped(jumps=ti).state
+        return
