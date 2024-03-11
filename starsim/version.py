25bf8f6949074dbe5f19d4c27b62f501f015de62
Keith Alexander Ashe <31802332+kaa2102@users.noreply.github.com>
2024-03-11 10:28:34 -0400

    Empty github-repo-stats Directory
    github-repo-stats currently mirros the main branch. Best practices are to create an empty branch.

--- starsim/version.py
+++
@@ -1,9 +0,0 @@
-'''
-Version and license information.
-'''
-
-__all__ = ['__version__', '__versiondate__', '__license__']
-
-__version__ = '0.2.6'
-__versiondate__ = '2024-02-29'
-__license__ = f'Starsim {__version__} ({__versiondate__}) — © 2023-2024 by IDM'
