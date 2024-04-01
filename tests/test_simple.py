085ad96ec86e26baa48f8574606cfdc14acc36d3
Cliff Kerr <questionably@gmail.com>
2024-04-01 18:24:39 -0400

    add additional multisim test

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -14,15 +14,15 @@ sc.options(interactive=False) # Assume not running interactively
 
 
 def make_sim_pars():
-    pars = dict(
+    pars = sc.objdict(
         n_agents = n_agents,
         birth_rate = 20,
         death_rate = 15,
-        networks = dict(
+        networks = sc.objdict(
             type = 'randomnet',
             n_contacts = 4,
         ),
-        diseases = dict(
+        diseases = sc.objdict(
             type = 'sir',
             dur_inf = 10,
             beta = 0.1,
@@ -144,10 +144,21 @@ def test_components(do_plot=do_plot):
 def test_parallel():
     """ Test running two identical sims in parallel """
     pars = make_sim_pars()
+
+    # Check that two identical sims match
     sims = ss.MultiSim([ss.Sim(pars, label='Sim1'), ss.Sim(pars, label='Sim2')])
     sims.run(keep_people=True)
     s1, s2 = sims.sims
     assert np.allclose(s1.summary[:], s2.summary[:], rtol=0, atol=0, equal_nan=True)
+
+    # Check that two non-identical sims don't match
+    pars2 = sc.dcp(pars)
+    pars2.diseases.beta *= 2
+    sims = ss.MultiSim([ss.Sim(pars, label='Sim1'), ss.Sim(pars2, label='Sim2')])
+    sims.run(keep_people=True)
+    s1, s2 = sims.sims
+    assert not np.allclose(s1.summary[:], s2.summary[:], rtol=0, atol=0, equal_nan=True)
+
     return s1, s2
 
 
