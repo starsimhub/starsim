e4f0574442a2964bff740a7e5511a4460bb839da
Keith Alexander Ashe <31802332+kaa2102@users.noreply.github.com>
2024-04-11 13:59:34 -0400

    Update test_dynamicnet.py

--- tests/test_dynamicnet.py
+++ tests/test_dynamicnet.py
@@ -5,14 +5,31 @@ Larger test of sim performance.
 
 import sciris as sc
 import starsim as ss
+import networkx as nx
+import numpy as np
+import pandas as pd 
+
 
 # Define the parameters
 #ss.options(multirng=True) #MultiRNG
 
 repeats = 10
-n_agents = 10_000
+n_agents = 1_000
 
-def make_run_sim():
+# Vary the infection duration from 1 to 100 days
+duration = [1,
+    2,
+    5,
+    10,
+    15,
+    25,
+    50,
+    75,
+    90,
+    100
+    ]
+
+def make_run_sim(duration,r):
     """ Make and run a decent-sized default simulation """
     # Make the people
     ppl = ss.People(n_agents=n_agents)
@@ -22,26 +39,32 @@ def make_run_sim():
     ]
 
     sir = ss.SIR()
-    ss.SIR(pars=dict( dur_inf=10, beta=0.2, init_prev=0.4, p_death=0.2))
+    ss.SIR(pars=dict( dur_inf=duration[r], beta=0.2, init_prev=0.4, p_death=0.2))
     networks=ss.DynamicNetwork()
-    
+
+
     # Make the sim
     sim = ss.Sim(people=ppl, networks=networks, demographics=demographics, diseases=sir)
 
     # Run the sim
     sim.run()
     
-    #Check we don't have more births than pregnancies
-    assert sum(sim.results.pregnancy.births) <= sum(sim.results.pregnancy.pregnancies)
-    
     return sim
 
 
 if __name__ == '__main__':
     
+    edge_count=[]    
     T = sc.timer()
-    for r in range(repeats):
-        make_run_sim()
+    cpr = sc.cprofile()
+    cpr.start()
+    for r in range(repeats):    
+        make_run_sim(duration, r)
         T.tt(f'Trial {r+1}/{repeats}')      
 
     sc.heading(f'Average: {T.mean()*1000:0.0f} ± {T.std()/len(T)**0.5*1000:0.0f} ms')
+    cpr.stop()
+    df=cpr.to_df()
+    df.to_csv('DynamicNet Test Profile.csv', index=False)
+    print("Run "+str(repeats)+" Mem "+str(sc.memload()))
+    print("RAM "+str(repeats)+" "+str(sc.checkram()))
