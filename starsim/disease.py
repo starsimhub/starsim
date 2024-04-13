bd6cfe943f835e3e142b6254cf9f3ec252180034
Cliff Kerr <questionably@gmail.com>
2024-04-13 22:09:09 +0300

    deaths not being handled correctly

--- starsim/disease.py
+++ starsim/disease.py
@@ -270,8 +270,8 @@ class Infection(Disease):
 
             nbetas = betamap[nkey]
             contacts = net.contacts
-            rel_trans = (self.infectious & people.alive) * self.rel_trans
-            rel_sus = (self.susceptible & people.alive) * self.rel_sus
+            rel_trans = self.infectious * self.rel_trans
+            rel_sus = self.susceptible * self.rel_sus
             p1p2b0 = [contacts.p1, contacts.p2, nbetas[0]]
             p2p1b1 = [contacts.p2, contacts.p1, nbetas[1]]
             for src, trg, beta in [p1p2b0, p2p1b1]:
