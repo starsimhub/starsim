6f2e16501ad691de626c856f775ff093216b49a2
Robyn Stuart <robyna.s@gmail.com>
2024-04-16 15:51:36 +0300

    change transmission logic

--- starsim/diseases/ebola.py
+++ starsim/diseases/ebola.py
@@ -137,6 +137,12 @@ class Ebola(SIR):
         self.ti_buried[safe_buried] = self.ti_dead[safe_buried]
         self.ti_buried[unsafe_buried] = self.ti_dead[unsafe_buried] + p.dur_dead2buried.rvs(unsafe_buried) / sim.dt
 
+        # Change rel_trans values
+        self.rel_trans[self.infectious] = 1
+        self.rel_trans[self.severe] = self.pars['sev_factor']  # Change for severe
+        unburied_uids = ss.true((self.ti_dead <= sim.ti) & (self.ti_buried > sim.ti))
+        self.rel_trans[unburied_uids] = self.pars['unburied_factor']  # Change for unburied
+
         return
 
     def update_death(self, sim, uids):
@@ -145,12 +151,12 @@ class Ebola(SIR):
             self.statesdict[state][uids] = False
         return
 
-    def make_new_cases(self, sim):
-        self.rel_trans[self.infectious] = 1
-        self.rel_trans[self.severe] = self.pars['sev_factor']
-
-        # Unburied UIDs
-        unburied_uids = ss.true((self.ti_dead <= sim.ti) & (self.ti_buried > sim.ti))
-        self.rel_trans[unburied_uids] = self.pars['unburied_factor']
-        super().make_new_cases(sim)
-        return
+    # def make_new_cases(self, sim):
+    #     self.rel_trans[self.infectious] = 1
+    #     self.rel_trans[self.severe] = self.pars['sev_factor']
+    #
+    #     # Unburied UIDs
+    #     unburied_uids = ss.true((self.ti_dead <= sim.ti) & (self.ti_buried > sim.ti))
+    #     self.rel_trans[unburied_uids] = self.pars['unburied_factor']
+    #     super().make_new_cases(sim)
+    #     return
