15770f8c7717759f283a2a5f4d8546132bc88503
Robyn Stuart <robyna.s@gmail.com>
2024-05-08 10:47:41 -0400

    update devtest;

--- tests/devtests/devtest_connectors.py
+++ tests/devtests/devtest_connectors.py
@@ -3,8 +3,9 @@ Experiment with connectors; see tut_diseases.ipynb
 """
 
 # %% Imports and settings
+import sciris as sc
 import starsim as ss
-import matplotlib.pyplot as plt
+import pylab as pl
 
 class simple_hiv_ng(ss.Connector):
     """ Simple connector whereby rel_sus to NG doubles if CD4 count is <200"""
@@ -27,29 +28,30 @@ class simple_hiv_ng(ss.Connector):
         sim.people.gonorrhea.rel_trans[sim.people.hiv.cd4 < 200] = self.pars.rel_trans_aids
         return
 
-ng = ss.Gonorrhea()
-ng.pars['beta'] = {'mf': [0.05, 0.025]}
-ng.pars['init_prev'] = 0.025
+# Make HIV
+hiv = ss.HIV(
+    beta = {'mf': [0.0008, 0.0004]},  # Specify transmissibility over the MF network
+    init_prev = 0.05,
+)
 
-ppl1 = ss.People(10000)
-sim_nohiv = ss.Sim(people=ppl1, networks=ss.MFNet(), diseases=ng)
+# Make gonorrhea
+ng = ss.Gonorrhea(
+    beta = {'mf': [0.05, 0.025]},  # Specify transmissibility over the MF network
+    init_prev = 0.025,
+)
+
+# Make the sim, including a connector betweeh HIV and gonorrhea:
+n_agents = 5_000
+sim = ss.Sim(n_agents=n_agents, networks='mf', diseases=[hiv, ng], connectors=simple_hiv_ng())
+sim.run()
+sim_nohiv = ss.Sim(n_agents=n_agents, networks='mf', diseases=ng)
 sim_nohiv.run()
 
-hiv = ss.HIV()
-hiv.pars['beta'] = {'mf': [0.0008, 0.0004]}
-hiv.pars['init_prev'] = 0.05
-ng = ss.Gonorrhea()
-ng.pars['beta'] = {'mf': [0.05, 0.025]}
-ng.pars['init_prev'] = 0.025
-ppl2 = ss.People(10000)
-sim_hiv = ss.Sim(people=ppl2, networks=ss.MFNet(), diseases=[hiv, ng], connectors=simple_hiv_ng())
-sim_hiv.run()
-
-plt.figure()
-plt.plot(sim_hiv.yearvec, sim_hiv.results.gonorrhea.n_infected, label='With HIV')
-plt.plot(sim_nohiv.yearvec, sim_nohiv.results.gonorrhea.n_infected, label='Without HIV')
-plt.title('Gonorrhea infections')
-plt.xlabel('Year')
-plt.ylabel('Count')
-plt.legend()
-plt.show()
+pl.figure()
+pl.plot(sim.yearvec, sim.results.gonorrhea.n_infected, label='With HIV')
+pl.plot(sim_nohiv.yearvec, sim_nohiv.results.gonorrhea.n_infected, label='Without HIV')
+pl.title('Gonorrhea infections')
+pl.xlabel('Year')
+pl.ylabel('Count')
+pl.legend()
+pl.show()
