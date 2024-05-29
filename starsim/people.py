c4a4c88a716ec5e31d931123d28d7fd6f8763667
Keith Alexander Ashe <31802332+kaa2102@users.noreply.github.com>
2024-05-29 11:15:14 -0400

    Update people.py
    Adds extra state 'mother_uids' with default=0 as FloatArr. Values updated with birthing mothers' uid. Note: IndexArr generated indexing errors.

--- starsim/people.py
+++ starsim/people.py
@@ -61,6 +61,7 @@ class People(sc.prettyobj):
         states = [
             ss.BoolArr('alive', default=True),  # Time index for death
             ss.BoolArr('female', default=ss.bernoulli(name='female', p=0.5)),
+            ss.FloatArr('mother_uids', default=0), # Array of Mother UIDs
             ss.FloatArr('age', default=self.get_age_dist(age_data)), # NaN until conceived
             ss.FloatArr('ti_dead'),  # Time index for death
             ss.FloatArr('scale', default=1.0), # The scale factor for the agents (multiplied for making results)
