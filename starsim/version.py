7d4a9acdded8bde73172c7335f3fc02524afbf9c
Cliff Kerr <questionably@gmail.com>
2024-07-05 20:08:10 -0400

    update version

--- starsim/version.py
+++ starsim/version.py
@@ -1,9 +1,9 @@
-'''
+"""
 Version and license information.
-'''
+"""
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '0.5.10'
-__versiondate__ = '2024-07-03'
+__version__ = '1.0.0'
+__versiondate__ = '2024-07-08'
 __license__ = f'Starsim {__version__} ({__versiondate__}) — © 2023-2024 by IDM'
