Alternate HIV implementation plus two bug fixes: * Values normalization in distribution.py * rand_seed setting in run.py

starsim/diseases/hiv.py
@@ -15,58 +15,54 @@ class HIV(ss.Infection):
         super().__init__()
         self.default_pars(
             beta = 1.0, # Placeholder value
-            cd4_min = 100,
-            cd4_max = 500,
-            cd4_rate = 5,
-            eff_condoms = 0.7,
             art_efficacy = 0.96,
+            VMMC_efficacy = 0.6,
             init_prev = ss.bernoulli(p=0.05),
-            death_dist = ss.bernoulli(p=self.death_prob_func), # Uses p_death by default, modulated by CD4
-            p_death = 0.05,
+            survival_without_art = ss.weibull(c=2, scale=13),
+            #survival_without_art = ss.weibull(c=2, scale=lambda self, sim, uids: 21.182-0.2717*sim.people.age[uids]), # Adult survival from EMOD
         )
         self.update_pars(pars=pars, **kwargs)
 
         # States
         self.add_states(
             ss.BoolArr('on_art', label='On ART'),
+            ss.BoolArr('circumcised', label='Circumcised'),
             ss.FloatArr('ti_art', label='Time of ART initiation'),
             ss.FloatArr('ti_dead', label='Time of death'), # Time of HIV-caused death
-            ss.FloatArr('cd4', default=500, label='CD4 count'),
         )
         return
-
-    @staticmethod
-    def death_prob_func(module, sim, uids):
-        p = module.pars
-        out = sim.dt * p.p_death / (p.cd4_min - p.cd4_max)**2 *  (module.cd4[uids] - p.cd4_max)**2
-        out = np.array(out)
-        return out
 
     def update_pre(self):
         """ Update CD4 """
         people = self.sim.people
-        self.cd4[people.alive & self.infected & self.on_art] += (self.pars.cd4_max - self.cd4[people.alive & self.infected & self.on_art])/self.pars.cd4_rate
-        self.cd4[people.alive & self.infected & ~self.on_art] += (self.pars.cd4_min - self.cd4[people.alive & self.infected & ~self.on_art])/self.pars.cd4_rate
 
-        self.rel_trans[people.alive & self.infected & self.on_art] = 1 - self.pars['art_efficacy']
+        self.rel_trans[self.infected & self.on_art] = 1 - self.pars['art_efficacy']
+        self.rel_sus[people.male & self.circumcised] = 1 - self.pars['VMMC_efficacy']
 
-        can_die = people.hiv.infected.uids
-        hiv_deaths = self.pars.death_dist.filter(can_die)
-        
+        hiv_deaths = (self.ti_dead == self.sim.ti).uids
         people.request_death(hiv_deaths)
-        self.ti_dead[hiv_deaths] = self.sim.ti
         return
 
     def init_results(self):
         """ Initialize results """
         super().init_results()
         self.results += ss.Result(self.name, 'new_deaths', self.sim.npts, dtype=int, label='Deaths')
+        self.results += ss.Result(self.name, 'art_coverage', self.sim.npts, dtype=float, label='ART Coverage')
+        self.results += ss.Result(self.name, 'vmmc_coverage', self.sim.npts, dtype=float, label='VMMC Coverage')
+        self.results += ss.Result(self.name, 'vmmc_coverage_15_49', self.sim.npts, dtype=float, label='VMMC Coverage 15-49')
+        self.results += ss.Result(self.name, 'prevalence_15_49', self.sim.npts, dtype=float, label='Prevalence 15-49')
         return
 
     def update_results(self):
         super().update_results()
         ti = self.sim.ti
         self.results['new_deaths'][ti] = np.count_nonzero(self.ti_dead == ti)
+        n_inf = np.count_nonzero(self.infected)
+        self.results['art_coverage'][ti] = np.count_nonzero(self.on_art) / n_inf if n_inf > 0 else 0
+        self.results['vmmc_coverage'][ti] = np.count_nonzero(self.circumcised) / np.count_nonzero(self.sim.people.male)
+        inds = (self.sim.people.age >= 15) & (self.sim.people.age < 50)
+        self.results['vmmc_coverage_15_49'][ti] = np.count_nonzero(self.circumcised[inds]) / np.count_nonzero(self.sim.people.male[inds])
+        self.results['prevalence_15_49'][ti] = np.count_nonzero(self.infected[inds]) / np.count_nonzero(self.sim.people.alive[inds])
         return 
 
     def set_prognoses(self, uids, source_uids=None):
@@ -74,6 +70,9 @@ class HIV(ss.Infection):
         self.susceptible[uids] = False
         self.infected[uids] = True
         self.ti_infected[uids] = self.sim.ti
+
+        prog = self.pars.survival_without_art.rvs(uids)
+        self.ti_dead[uids] = self.sim.ti + np.round(prog/self.sim.dt).astype(int) # Survival without treatment
         return
 
     def set_congenital(self, uids, source_uids):
@@ -90,16 +89,27 @@ class ART(ss.Intervention):
         self.coverage = sc.toarray(coverage)
         super().__init__()
         self.default_pars(
-            art_delay = ss.constant(v=1) # Value in years
+        #    art_delay = ss.lognorm_im(mean=1) # Value in years
         )
         self.update_pars(pars=pars, **kwargs)
 
-        prob_art = lambda self, sim, uids: np.interp(sim.year, self.year, self.coverage)
-        self.prob_art_at_infection = ss.bernoulli(p=prob_art)
+        prob_art_at_init = lambda self, sim, uids: np.interp(sim.year, self.year, self.coverage)
+        self.prob_art_at_infection = ss.bernoulli(p=prob_art_at_init)
+        self.prob_art_post_infection = ss.bernoulli(p=0) # Set in init_pre
         return
 
     def init_pre(self, sim):
         super().init_pre(sim)
+
+        # Importance sampling
+        cvec = np.interp(sim.yearvec, self.year, self.coverage)
+        pvec = np.zeros_like(cvec)
+        np.divide(np.diff(cvec), 1-cvec[:-1], where=cvec[:-1]<1, out=pvec[1:])
+        pvec = np.clip(pvec, a_min=0, a_max=1)
+
+        prob_art_post_init = lambda self, sim, uids, pvec=pvec: pvec[sim.ti]
+        self.prob_art_post_infection.set(p=prob_art_post_init)
+
         self.results += ss.Result(self.name, 'n_art', sim.npts, dtype=int)
         self.initialized = True
         return
@@ -110,18 +120,92 @@ class ART(ss.Intervention):
 
         hiv = sim.people.hiv
         infected = hiv.infected.uids
-        ti_delay = np.round(self.pars.art_delay.rvs(infected)/sim.dt).astype(int)
-        recently_infected = infected[hiv.ti_infected[infected] == sim.ti-ti_delay]
+        ti_delay = 1 + np.round(2/sim.dt).astype(int) # About 2 years to ART initiation, 1+ due to order of operations, we're on the next time step
+        recently_infected = infected[hiv.ti_infected[infected] == sim.ti - ti_delay]
+        notrecent_noart = infected[(hiv.ti_infected[infected] < sim.ti - ti_delay) & (~hiv.on_art[infected])]
 
         n_added = 0
-        if len(recently_infected) > 0:
+        if len(recently_infected):
             inds = self.prob_art_at_infection.filter(recently_infected)
             hiv.on_art[inds] = True
             hiv.ti_art[inds] = sim.ti
-            n_added = len(inds)
+            hiv.ti_dead[inds] = np.nan
+            n_added += len(inds)
 
-        # Add result
+        if len(notrecent_noart):
+            inds = self.prob_art_post_infection.filter(notrecent_noart)
+            hiv.on_art[inds] = True
+            hiv.ti_art[inds] = sim.ti
+            hiv.ti_dead[inds] = np.nan
+            n_added += len(inds)
+
+        # Add results
         self.results['n_art'][sim.ti] = np.count_nonzero(hiv.on_art)
+
+        return n_added
+
+
+class VMMC(ss.Intervention):
+
+    def __init__(self, year, coverage, pars=None, **kwargs):
+        self.requires = HIV
+        self.year = sc.toarray(year)
+        self.coverage = sc.toarray(coverage)
+        super().__init__()
+        self.default_pars()
+        self.update_pars(pars=pars, **kwargs)
+
+        prob_VMMC_at_debut = lambda self, sim, uids: np.interp(sim.year, self.year, self.coverage)
+        self.prob_VMMC_at_debut = ss.bernoulli(p=prob_VMMC_at_debut)
+        self.prob_VMMC_post_debut = ss.bernoulli(p=0) # Set in init_pre
+        return
+
+    def init_pre(self, sim):
+        super().init_pre(sim)
+
+        # Importance sampling
+        cvec = np.interp(sim.yearvec, self.year, self.coverage)
+        pvec = np.zeros_like(cvec)
+        np.divide(np.diff(cvec), 1-cvec[:-1], where=cvec[:-1]<1, out=pvec[1:])
+        pvec = np.clip(pvec, a_min=0, a_max=1)
+
+        p_vmmc = lambda self, sim, uids, pvec=pvec: pvec[sim.ti]
+        self.prob_VMMC_post_debut.set(p=p_vmmc)
+
+        self.results += ss.Result(self.name, 'n_vmmc', sim.npts, dtype=int)
+        self.initialized = True
+        return
+
+    def apply(self, sim):
+        if sim.year < self.year[0]:
+            return
+
+        hiv = sim.people.hiv
+        debut = None
+        for net in sim.networks.values():
+            if hasattr(net, 'debut'):
+                debut = net.debut
+                break # Take first
+        assert debut != None
+        recent_debut = (self.sim.people.age >= debut) & (self.sim.people.age < debut + sim.dt) & (self.sim.people.male) & (~hiv.circumcised)
+        male_novmmc = (~recent_debut) & (self.sim.people.male) & (~hiv.circumcised)
+
+        n_added = 0
+
+        if recent_debut.any():
+            inds = self.prob_VMMC_at_debut.filter(recent_debut)
+            hiv.circumcised[inds] = True
+            #hiv.ti_circumcised[inds] = sim.ti
+            n_added += len(inds)
+
+        if male_novmmc.any():
+            inds = self.prob_VMMC_post_debut.filter(male_novmmc)
+            hiv.circumcised[inds] = True
+            #hiv.ti_circumcised[inds] = sim.ti
+            n_added += len(inds)
+
+        # Add results
+        self.results['n_vmmc'][sim.ti] = n_added
 
         return n_added
 