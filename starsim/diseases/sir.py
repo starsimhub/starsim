4e59fc3ffb4599bfd53700dc5ca4e01b683354ea
Cliff Kerr <questionably@gmail.com>
2024-09-11 20:22:06 -0400

    starting to work on partial version

--- starsim/diseases/sir.py
+++ starsim/diseases/sir.py
@@ -18,6 +18,34 @@ class SIR(ss.Infection):
     infected/infectious, and recovered. It also includes deaths, and basic
     results.
     """
+    # def __init__(self, pars=None, **kwargs):
+    #     super().__init__()
+    #     self.define_pars(
+    #         beta = 0.1,
+    #         init_prev = ss.bernoulli(p=0.01),
+    #         dur_inf = ss.lognorm_ex(mean=6),
+    #         p_death = ss.bernoulli(p=0.01),
+    #     )
+    #     self.update_pars(pars, **kwargs)
+        
+    #     self.define_states(
+    #         ss.State('susceptible', True),
+    #         ss.State('infected'),
+    #         ss.State('recovered'),
+    #     )
+        
+    #     self.define_events(
+    #         ss.Event('susceptible -> infected', func=self.infect,  reskey='infections'),
+    #         ss.Event('infected -> recovered',   func=self.recover, reskey='recoveries'),
+    #         ss.Event('infected -> dead',        func=self.die,     reskey='deaths'),
+    #     )
+
+    #     self.define_attrs(
+    #         ss.FloatArr('rel_sus',   default=1.0),
+    #         ss.FloatArr('rel_trans', default=1.0),
+    #     )
+    #     return
+    
     def __init__(self, pars=None, **kwargs):
         super().__init__()
         self.define_pars(
@@ -29,24 +57,18 @@ class SIR(ss.Infection):
         self.update_pars(pars, **kwargs)
         
         self.define_states(
-            ss.State('susceptible', True),
-            ss.State('infected'),
-            ss.State('recovered'),
-        )
-        
-        self.define_events(
-            ss.Event('susceptible -> infected', func=self.infect,  reskey='infections'),
-            ss.Event('infected -> recovered',   func=self.recover, reskey='recoveries'),
-            ss.Event('infected -> dead',        func=self.die,     reskey='deaths'),
-        )
-
-        self.define_attrs(
-            ss.FloatArr('rel_sus',   default=1.0),
-            ss.FloatArr('rel_trans', default=1.0),
+            ss.BoolArr('susceptible', default=True, label='Susceptible'),
+            ss.BoolArr('infected', label='Infectious'),
+            ss.BoolArr('recovered', label='Recovered'),
+            ss.FloatArr('ti_infected', label='Time of infection'),
+            ss.FloatArr('ti_recovered', label='Time of recovery'),
+            ss.FloatArr('ti_dead', label='Time of death'),
+            ss.FloatArr('rel_sus', default=1.0, label='Relative susceptibility'),
+            ss.FloatArr('rel_trans', default=1.0, label='Relative transmission'),
         )
         return
 
-    def update_pre(self):
+    def step_pre(self):
         # Progress infectious -> recovered
         sim = self.sim
         recovered = (self.infected & (self.ti_recovered <= sim.ti)).uids
