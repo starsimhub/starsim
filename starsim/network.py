1a46c6d25e91ee3310c6eaaadee920856e7cd3df
Cliff Kerr <questionably@gmail.com>
2024-09-22 02:09:11 -0400

    add more units, 2 failed

--- starsim/network.py
+++ starsim/network.py
@@ -700,7 +700,7 @@ class MFNet(SexualNetwork):
     def __init__(self, pars=None, key_dict=None, **kwargs):
         super().__init__(key_dict=key_dict)
         self.define_pars(
-            duration = ss.lognorm_ex(mean=15),  # Can vary by age, year, and individual pair. Set scale=exp(mu) and s=sigma where mu,sigma are of the underlying normal distribution.
+            duration = ss.lognorm_ex(mean=ss.dur(15)),  # Can vary by age, year, and individual pair. Set scale=exp(mu) and s=sigma where mu,sigma are of the underlying normal distribution.
             participation = ss.bernoulli(p=0.9),  # Probability of participating in this network - can vary by individual properties (age, sex, ...) using callable parameter values
             debut = ss.normal(loc=16),  # Age of debut can vary by using callable parameter values
             acts = ss.poisson(lam=80),
