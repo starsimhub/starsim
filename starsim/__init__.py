201dc0ff1c487422f40656cecd78a16df8bb6c1e
Cliff Kerr <questionably@gmail.com>
2024-09-22 21:56:39 -0400

    rename timeunits to timepars

--- starsim/__init__.py
+++ starsim/__init__.py
@@ -1,7 +1,7 @@
 from .version import __version__, __versiondate__, __license__
 from .settings      import *
 from .utils         import *
-from .timeunits     import *
+from .timepars      import *
 from .parameters    import *
 from .distributions import *
 from .states        import *
--- starsim/diseases/sir.py
+++ starsim/diseases/sir.py
@@ -37,16 +37,16 @@ class SIR(ss.Infection):
         
     #     # Option 1 for defining events -- using objects
     #     self.define_events(
-    #         ss.Event(self.susceptible, self.infected, func=self.infect,  reskey='infections'),
-    #         ss.Event(self.infected, self.recovered,   func=self.recover, reskey='recoveries'),
-    #         ss.Event(self.infected, self.dead,        func=self.step_die,     reskey='deaths'),
+    #         ss.Event(self.susceptible, self.infected, func=self.infect,   reskey='infections'),
+    #         ss.Event(self.infected, self.recovered,   func=self.recover,  reskey='recoveries'),
+    #         ss.Event(self.infected, self.dead,        func=self.step_die, reskey='deaths'),
     #     )
         
     #     # Option 2 for defining events -- using strings
     #     self.define_events(
-    #         ss.Event('susceptible -> infected', self.infect,  reskey='infections'),
-    #         ss.Event('infected -> recovered',   self.recover, reskey='recoveries'),
-    #         ss.Event('infected -> dead',        self.step_die,     reskey='deaths'),
+    #         ss.Event('susceptible -> infected', self.infect,   reskey='infections'),
+    #         ss.Event('infected -> recovered',   self.recover,  reskey='recoveries'),
+    #         ss.Event('infected -> dead',        self.step_die, reskey='deaths'),
     #     )
         
     #     self.define_attrs(
