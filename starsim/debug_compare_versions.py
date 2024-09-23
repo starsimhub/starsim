a14a799a2d93d6a920f7af6312a584e278fbe048
Cliff Kerr <questionably@gmail.com>
2024-09-23 14:53:36 -0400

    sir and sis match with random

--- starsim/debug_compare_versions.py
+++ starsim/debug_compare_versions.py
@@ -1,18 +1,20 @@
 import sciris as sc
 import starsim as ss
 
-debug = 0
+debug = 1
 
 # Define the parameters
 pars = sc.objdict(
     dur        = 20,
     n_agents   = 10e3,
-    dt         = 0.2,
+    dt         = 0.1,
     rand_seed  = 2,
     verbose    = 0.02,
-    diseases   = ['sir'],
+    diseases   = ['sir', 'sis'],
     networks   = ['random']#, 'mf', 'maternal'],
 )
+
+# s1 = ss.Sim(dur=20, diseases=['sir'], networks=['random'], dt=0.2, rand_seed=2)
 
 s1 = ss.Sim(pars)
 
