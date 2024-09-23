a91d1737827370981b7ceaa45fb65c369230ebff
Cliff Kerr <questionably@gmail.com>
2024-09-23 14:32:21 -0400

    sir-random does not match

--- starsim/debug_compare_versions.py
+++ starsim/debug_compare_versions.py
@@ -1,7 +1,7 @@
 import sciris as sc
 import starsim as ss
 
-debug = 1
+debug = 0
 
 # Define the parameters
 pars = sc.objdict(
@@ -10,8 +10,8 @@ pars = sc.objdict(
     dt         = 0.2,
     rand_seed  = 2,
     verbose    = 0.02,
-    diseases   = ['sir', 'sis'],
-    networks   = ['random', 'mf', 'maternal'],
+    diseases   = ['sir'],
+    networks   = ['random']#, 'mf', 'maternal'],
 )
 
 s1 = ss.Sim(pars)
@@ -19,6 +19,7 @@ s1 = ss.Sim(pars)
 if debug:
     s1.run()
     s1.plot()
+    print(s1.summary)
     
 else:
     with sc.timer():
@@ -26,6 +27,6 @@ else:
         m1 = ss.MultiSim(s1).run(n_runs=n)
         new = m1.summarize()
         folder = '/home/cliffk/idm/starsim2/tests/'
-        old = sc.loadjson(f'{folder}tmp_random.json')
+        old = sc.loadjson(f'{folder}tmp_baseline.json')
         df = ss.diff_sims(old, new, output=True)
         df.disp()
