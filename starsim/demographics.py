fix crude birth rate calculation

starsim/demographics.py
@@ -105,14 +105,19 @@ class Births(Demographics):
         return new_uids
 
     def update_results(self):
-        self.results['new'][self.ti] = self.n_births
+        # New births -- already calculated
+        self.results.new[self.ti] = self.n_births
+
+        # Calculate crude birth rate (CBR)
+        inv_units = 1.0/self.pars.units
+        births_per_year = self.n_births/self.sim.dt_year
+        denom = self.sim.results.n_alive[self.sim.ti]
+        self.results.cbr[self.ti] = inv_units*births_per_year/denom
         return
 
     def finalize(self):
         super().finalize()
-        res = self.sim.results
         self.results.cumulative[:] = np.cumsum(self.results.new)
-        self.results.cbr[:] = 1/self.pars.units*np.divide(self.results.new/self.sim.dt_year, res.n_alive, where=res.n_alive>0)
         return
 
 
@@ -502,7 +507,6 @@ class Pregnancy(Demographics):
 
         # Change states for the newly pregnant woman
         ti = self.ti
-        dt = self.dt # TODO: CHECK
         self.fecund[uids] = False
         self.pregnant[uids] = True
         self.ti_pregnant[uids] = ti

tests/test_time.py
@@ -81,10 +81,23 @@ def test_classes():
 
 def test_units(do_plot=False):
     sc.heading('Test behavior of year vs day units')
-    
+
+    sis = ss.SIS(
+        beta = ss.beta(0.05, 'day'),
+        init_prev = ss.bernoulli(p=0.1),
+        dur_inf = ss.lognorm_ex(mean=ss.dur(10, 'day')),
+        waning = ss.rate(0.05, 'day'),
+        imm_boost = 1.0,
+    )
+
+    rnet = ss.RandomNet(
+        n_contacts = 10,
+        dur = 0, # Note; network edge durations are required to have the same unit as the network
+    )
+
     pars = dict(
-        diseases = dict(type='sis', init_prev=0.1),
-        networks = 'random',
+        diseases = sis,
+        networks = rnet,
         n_agents = small,
     )
     
@@ -97,10 +110,10 @@ def test_units(do_plot=False):
         if do_plot:
             sim.plot()
     
-    # Uncomment this test once it might potentially pass lol
-    rtol = 0.01
+    # Check that results match to within stochastic uncertainty
+    rtol = 0.05
     vals = [sim.summary.sis_cum_infections for sim in [sims.y, sims.d]]
-    # assert np.isclose(*vals, rtol=rtol), f'Values for cum_infections do not match ({vals})'
+    assert np.isclose(*vals, rtol=rtol), f'Values for cum_infections do not match ({vals})'
         
     return sims
 
@@ -110,7 +123,7 @@ def test_multi_timestep(do_plot=False):
     
     pars = dict(
         diseases = ss.SIS(unit='day', dt=1.0, init_prev=0.1, beta=ss.beta(0.01)),
-        # demographics = ss.Births(unit='year', dt=0.25),
+        demographics = ss.Births(unit='year', dt=0.25),
         networks = ss.RandomNet(unit='week'),
         n_agents = small,
     )