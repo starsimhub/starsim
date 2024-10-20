aac65c502a37bc85c06f214fbc099ac86af56840
Daniel Klein <daniel.klein@gatesfoundation.org>
2024-10-19 23:13:52 -0700

    Starting work on a CalibComponent. WIP

--- starsim/calibration.py
+++ starsim/calibration.py
@@ -11,98 +11,7 @@ import starsim as ss
 import datetime as dt
 
 
-__all__ = ['Calibration', 'compute_gof']
-
-
-def compute_gof(actual, predicted, normalize=True, use_frac=False, use_squared=False,
-                as_scalar='none', eps=1e-9, skestimator=None, estimator=None, **kwargs):
-    """
-    Calculate the goodness of fit. By default use normalized absolute error, but
-    highly customizable. For example, mean squared error is equivalent to
-    setting normalize=False, use_squared=True, as_scalar='mean'.
-
-    Args:
-        actual      (arr):   array of actual (data) points
-        predicted   (arr):   corresponding array of predicted (model) points
-        normalize   (bool):  whether to divide the values by the largest value in either series
-        use_frac    (bool):  convert to fractional mismatches rather than absolute
-        use_squared (bool):  square the mismatches
-        as_scalar   (str):   return as a scalar instead of a time series: choices are sum, mean, median
-        eps         (float): to avoid divide-by-zero
-        skestimator (str):   if provided, use this scikit-learn estimator instead
-        estimator   (func):  if provided, use this custom estimator instead
-        kwargs      (dict):  passed to the scikit-learn or custom estimator
-
-    Returns:
-        gofs (arr): array of goodness-of-fit values, or a single value if as_scalar is True
-
-    **Examples**::
-
-        x1 = np.cumsum(np.random.random(100))
-        x2 = np.cumsum(np.random.random(100))
-
-        e1 = compute_gof(x1, x2) # Default, normalized absolute error
-        e2 = compute_gof(x1, x2, normalize=False, use_frac=False) # Fractional error
-        e3 = compute_gof(x1, x2, normalize=False, use_squared=True, as_scalar='mean') # Mean squared error
-        e4 = compute_gof(x1, x2, skestimator='mean_squared_error') # Scikit-learn's MSE method
-        e5 = compute_gof(x1, x2, as_scalar='median') # Normalized median absolute error -- highly robust
-    """
-
-    # Handle inputs
-    actual    = np.array(sc.dcp(actual), dtype=float)
-    predicted = np.array(sc.dcp(predicted), dtype=float)
-
-    # Scikit-learn estimator is supplied: use that
-    if skestimator is not None: # pragma: no cover
-        try:
-            import sklearn.metrics as sm
-            sklearn_gof = getattr(sm, skestimator) # Shortcut to e.g. sklearn.metrics.max_error
-        except ImportError as E:
-            errormsg = f'You must have scikit-learn >=0.22.2 installed: {str(E)}'
-            raise ImportError(errormsg) from E
-        except AttributeError as E:
-            errormsg = f'Estimator {skestimator} is not available; see https://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter for options'
-            raise AttributeError(errormsg) from E
-        gof = sklearn_gof(actual, predicted, **kwargs)
-        return gof
-
-    # Custom estimator is supplied: use that
-    if estimator is not None: # pragma: no cover
-        try:
-            gof = estimator(actual, predicted, **kwargs)
-        except Exception as E:
-            errormsg = f'Custom estimator "{estimator}" must be a callable function that accepts actual and predicted arrays, plus optional kwargs'
-            raise RuntimeError(errormsg) from E
-        return gof
-
-    # Default case: calculate it manually
-    else:
-        # Key step -- calculate the mismatch!
-        gofs = abs(np.array(actual) - np.array(predicted))
-
-        if normalize and not use_frac:
-            actual_max = abs(actual).max()
-            if actual_max > 0:
-                gofs /= actual_max
-
-        if use_frac:
-            if (actual<0).any() or (predicted<0).any():
-                print('Warning: Calculating fractional errors for non-positive quantities is ill-advised!')
-            else:
-                maxvals = np.maximum(actual, predicted) + eps
-                gofs /= maxvals
-
-        if use_squared:
-            gofs = gofs**2
-
-        if as_scalar == 'sum':
-            gofs = np.sum(gofs)
-        elif as_scalar == 'mean':
-            gofs = np.mean(gofs)
-        elif as_scalar == 'median':
-            gofs = np.median(gofs)
-
-        return gofs
+__all__ = ['Calibration', 'CalibComponent', 'compute_gof']
 
 
 class Calibration(sc.prettyobj):
@@ -587,3 +496,134 @@ class Calibration(sc.prettyobj):
             plt.ylabel('Mismatch')
         sc.figlayout()
         return fig
+
+
+from enum import Enum
+
+class eMode(Enum):
+    PREVALENT = 0
+    INCIDENT = 1
+
+class CalibComponent(sc.prettyobj):
+    """
+    A class to compare a single channel of observed data with output from a
+    simulation. The Calibration class can use several CalibComponent objects to
+    form an overall understanding of how will a given simulation reflects
+    observed data.
+
+    Args:
+        name (str) : the of this component. Importantly,
+            sim_extract_fn is None, the code will attempt to use the name, like
+            "hiv.prevalence" to automatically extract data from the simulation.
+        data (df) : pandas Series containing calibration data. The index should be the time in either floating point years or datetime.
+        mode (eMode): To handle misaligned timepoints between observed data and simulation output, it's important to know if the data are incident (like new cases) or prevalent (like the number infected).
+            If eMode.PREVALENT, simulation outputs will be interpolated to observed timepoints.
+            If eMode.INCIDENT, ...
+    """
+    def __init__(self, name, data, mode, likelihood, sim_extract_fn=None):
+        pass
+
+    def validate(self):
+        pass
+
+    def __call__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def plot(self):
+        pass
+
+
+
+def compute_gof(actual, predicted, normalize=True, use_frac=False, use_squared=False,
+                as_scalar='none', eps=1e-9, skestimator=None, estimator=None, **kwargs):
+    """
+    Calculate the goodness of fit. By default use normalized absolute error, but
+    highly customizable. For example, mean squared error is equivalent to
+    setting normalize=False, use_squared=True, as_scalar='mean'.
+
+    Args:
+        actual      (arr):   array of actual (data) points
+        predicted   (arr):   corresponding array of predicted (model) points
+        normalize   (bool):  whether to divide the values by the largest value in either series
+        use_frac    (bool):  convert to fractional mismatches rather than absolute
+        use_squared (bool):  square the mismatches
+        as_scalar   (str):   return as a scalar instead of a time series: choices are sum, mean, median
+        eps         (float): to avoid divide-by-zero
+        skestimator (str):   if provided, use this scikit-learn estimator instead
+        estimator   (func):  if provided, use this custom estimator instead
+        kwargs      (dict):  passed to the scikit-learn or custom estimator
+
+    Returns:
+        gofs (arr): array of goodness-of-fit values, or a single value if as_scalar is True
+
+    **Examples**::
+
+        x1 = np.cumsum(np.random.random(100))
+        x2 = np.cumsum(np.random.random(100))
+
+        e1 = compute_gof(x1, x2) # Default, normalized absolute error
+        e2 = compute_gof(x1, x2, normalize=False, use_frac=False) # Fractional error
+        e3 = compute_gof(x1, x2, normalize=False, use_squared=True, as_scalar='mean') # Mean squared error
+        e4 = compute_gof(x1, x2, skestimator='mean_squared_error') # Scikit-learn's MSE method
+        e5 = compute_gof(x1, x2, as_scalar='median') # Normalized median absolute error -- highly robust
+    """
+
+    # Handle inputs
+    actual    = np.array(sc.dcp(actual), dtype=float)
+    predicted = np.array(sc.dcp(predicted), dtype=float)
+
+    # Scikit-learn estimator is supplied: use that
+    if skestimator is not None: # pragma: no cover
+        try:
+            import sklearn.metrics as sm
+            sklearn_gof = getattr(sm, skestimator) # Shortcut to e.g. sklearn.metrics.max_error
+        except ImportError as E:
+            errormsg = f'You must have scikit-learn >=0.22.2 installed: {str(E)}'
+            raise ImportError(errormsg) from E
+        except AttributeError as E:
+            errormsg = f'Estimator {skestimator} is not available; see https://scikit-learn.org/stable/modules/model_evaluation.html#scoring-parameter for options'
+            raise AttributeError(errormsg) from E
+        gof = sklearn_gof(actual, predicted, **kwargs)
+        return gof
+
+    # Custom estimator is supplied: use that
+    if estimator is not None: # pragma: no cover
+        try:
+            gof = estimator(actual, predicted, **kwargs)
+        except Exception as E:
+            errormsg = f'Custom estimator "{estimator}" must be a callable function that accepts actual and predicted arrays, plus optional kwargs'
+            raise RuntimeError(errormsg) from E
+        return gof
+
+    # Default case: calculate it manually
+    else:
+        # Key step -- calculate the mismatch!
+        gofs = abs(np.array(actual) - np.array(predicted))
+
+        if normalize and not use_frac:
+            actual_max = abs(actual).max()
+            if actual_max > 0:
+                gofs /= actual_max
+
+        if use_frac:
+            if (actual<0).any() or (predicted<0).any():
+                print('Warning: Calculating fractional errors for non-positive quantities is ill-advised!')
+            else:
+                maxvals = np.maximum(actual, predicted) + eps
+                gofs /= maxvals
+
+        if use_squared:
+            gofs = gofs**2
+
+        if as_scalar == 'sum':
+            gofs = np.sum(gofs)
+        elif as_scalar == 'mean':
+            gofs = np.mean(gofs)
+        elif as_scalar == 'median':
+            gofs = np.median(gofs)
+
+        return gofs
+
