e3695765039722103b5e05b6fc78f8860c349847
Daniel Klein <daniel.klein@gatesfoundation.org>
2024-12-20 16:10:17 -0800

    Being more careful with `combine_kwargs`

--- starsim/calib_components.py
+++ starsim/calib_components.py
@@ -123,7 +123,9 @@ class CalibComponent(sc.prettyobj):
         self.n_boot = n_boot
 
         self.combine_reps = combine_reps
-        self.combine_kwargs = dict(numeric_only=True)
+        if isinstance(self.combine_reps, str) and hasattr(pd.core.groupby.DataFrameGroupBy, self.combine_reps):
+            # Most of these methods take numeric_only, which can help with stability
+            self.combine_kwargs = dict(numeric_only=True)
 
         self.avail_conforms = {
             'incident':  linear_accum,  # or self.linear_accum if left as staticmethod  
