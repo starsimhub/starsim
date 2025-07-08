8a6b24ac945bd905c745949088edb1e091d4bdfe
Cliff Kerr <questionably@gmail.com>
2025-07-07 20:55:28 -0400

    rename verbose to debug

--- starsim/__init__.py
+++ starsim/__init__.py
@@ -44,26 +44,26 @@ from .calib_components import *
 from .samples          import *
 
 # Load fonts
-def _load_fonts(verbose=False):
+def _load_fonts(debug=False):
     """ Try installing and loading fonts, but fail silently if folder is not writable """
     if options.install_fonts:
         test_file = root / 'starsim' / 'assets' / 'fonts_installed_successfully'
 
         # Check if fonts have already been installed
         if not test_file.exists():
-            if verbose: print(f'File {test_file} does not exist, continuing...')
+            if debug: print(f'File {test_file} does not exist, continuing...')
             try:
-                if verbose: print('Checking write permissions...')
+                if debug: print('Checking write permissions...')
                 test_file.touch()  # Mark that fonts were installed
-                if verbose: print('Rebuilding fonts...')
+                if debug: print('Rebuilding fonts...')
                 settings.load_fonts(rebuild=True)
                 if options.verbose:
                     print('\nNote: rebuilding the font cache only happens once on first import, or set the environment variable STARSIM_INSTALL_FONTS=0 to disable.')
             except Exception as E:
-                if verbose: print(f'File not writable or another error: {E}')
+                if debug: print(f'File not writable or another error: {E}')
                 pass  # Not writable or something else went wrong; ignore
         else:
-            if verbose: print(f'File {test_file} exists, loading fonts...')
+            if debug: print(f'File {test_file} exists, loading fonts...')
             settings.load_fonts(rebuild=False)
     return
 
