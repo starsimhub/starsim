c82f6baa367c503a55379e0dd7928471c4449964
Cliff Kerr <questionably@gmail.com>
2025-07-29 19:31:29 -0400

    almost working

--- docs/migration_v2v3/unit__v2.py
+++ docs/migration_v2v3/unit__v2.py
@@ -2,7 +2,7 @@ import starsim as ss
 
 pars = dict(
     diseases = ss.SIS(unit='day', dt=1.0, init_prev=0.1),
-    demographics = ss.Births(unit='year', dt=0.25),
+    demographics = ss.Births(dt=0.25, unit='year'),
     networks = ss.RandomNet(unit='week'),
 )
 
