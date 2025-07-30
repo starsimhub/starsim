ff71d5ab9015dde92930d8becb05b6453b4d7524
Cliff Kerr <questionably@gmail.com>
2025-07-30 19:44:17 -0400

    tidying

--- docs/user_guide/check_user_guides.py
+++ docs/user_guide/check_user_guides.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
-Run all the Jupyter notebook tutorials. Usage:
-  ./check_tutorials.py
+Run all the Jupyter notebook user guides. Usage:
+  ./check_user_guides.py
 
 See check_notebooks in the parent folder for more information.
 """
@@ -12,4 +12,4 @@ parent = sc.path(__file__).resolve().parent.parent # Ensure it's the real path i
 cnb = sc.importbypath(parent / 'check_notebooks.py')
 
 if __name__ == '__main__':
-    results = cnb.main(*sys.argv[1:], folders=['tutorials'])
+    results = cnb.main(*sys.argv[1:], folders=['user_guide'])
