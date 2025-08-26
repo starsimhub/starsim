1aa98d7ae40dcd301f122de3de960c6c3c903d7b
Robyn Stuart <robyna.s@gmail.com>
2025-08-26 15:16:16 +1000

    update changelog

--- starsim/version.py
+++ starsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '3.0.1'
-__versiondate__ = '2025-08-04'
+__version__ = '3.0.2'
+__versiondate__ = '2025-08-25'
 __license__ = f'Starsim {__version__} ({__versiondate__}) — © 2023-2025 by IDM'
