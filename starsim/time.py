a376aa061093f18fccec09fd5b57b85547798667
Romesh Abeysuriya <romesh.abey@gmail.com>
2025-11-18 14:39:49 +1100

    Remove old unused class

--- starsim/time.py
+++ starsim/time.py
@@ -2273,40 +2273,6 @@ class DateConverter(matplotlib.units.ConversionInterface):
         else:
             return DateConverter._convert_single(value)
 
-
-# __all__ += ['DateIndex']
-#
-# class DateIndex(pd.Index):
-#     """
-#     Custom Index that wraps DateArray and enables custom matplotlib behavior
-#
-#     This uses composition rather than multiple inheritance to avoid conflicts
-#     between DateArray (ndarray subclass) and Index (also has ndarray behavior).
-#     """
-#
-#         """ Lightweight wrapper for an array of dates """
-#         def __new__(cls, arr=None, unit=None):
-#             arr = sc.toarray(arr)
-#             if isinstance(arr, np.ndarray): # Shortcut to typical use case, where the input is an array
-#                 if unit is None:
-#                     if isinstance(arr[0], (ss.date, ss.dur)):
-#                         unit = type(arr[0]) # Get the unit from the input
-#                     elif isinstance(arr[0], (np.datetime64, dt.date, dt.datetime, pd.Timestamp)):
-#                         arr = np.fromiter((ss.date(x) for x in arr), dtype=object)
-#                         unit = ss.date
-#                     else:
-#                         try: # Else, if the input is datelike (>1), assume date
-#                             assert np.all(arr >= 1.0)
-#                             unit = ss.date
-#                         except: # Otherwise, assume years
-#                             unit = ss.years
-#                 out = arr.view(cls)
-#                 out._unit = unit
-#                 return out
-#             else:
-#                 errormsg = f'Argument must be an array, not {type(arr)}'
-#                 raise TypeError(errormsg)
-
 # Register the converter for the Starsim date classes
 matplotlib.units.registry[date] = DateConverter()
 matplotlib.units.registry[DateArray] = DateConverter()
