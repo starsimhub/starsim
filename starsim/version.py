15aff1468226dd9b5256d026e947d712d042325e
Romesh Abeysuriya <romesh.abey@gmail.com>
2026-01-28 13:02:30 +1100

    Update version

--- starsim/version.py
+++ starsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '3.1.0.dev0'
-__versiondate__ = '2025-11-27'
-__license__ = f'Starsim {__version__} ({__versiondate__}) — © 2023-2025 by IDM'
+__version__ = '3.0.6'
+__versiondate__ = '2026-01-28'
+__license__ = f'Starsim {__version__} ({__versiondate__}) — © 2023-2026 by IDM'
