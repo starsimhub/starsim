7b42ad71daf95733be1fb0636ac19add2bae9c9b
Cliff Kerr <questionably@gmail.com>
2026-02-18 21:08:29 -0500

    update version and add release procedure to contributing doc

--- starsim/version.py
+++ starsim/version.py
@@ -4,6 +4,6 @@ Version and license information.
 
 __all__ = ['__version__', '__versiondate__', '__license__']
 
-__version__ = '3.1.1.dev0'
-__versiondate__ = '2026-XX-XX'
+__version__ = '3.1.1'
+__versiondate__ = '2026-02-18'
 __license__ = f'Starsim {__version__} ({__versiondate__}) — © 2023-2026 by IDM'
