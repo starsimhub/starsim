1740f023a0af5836afb3601d79bb4575b49bacc5
Romesh Abeysuriya <romesh.abey@gmail.com>
2026-03-20 11:08:04 +1100

    Update df and json format to handle seed arrays

--- starsim/calibration.py
+++ starsim/calibration.py
@@ -528,49 +528,25 @@ class Calibration(sc.prettyobj):
 
     def _parse_study(self, study):
         """Parse the study into a data frame -- called automatically """
-        best = study.best_params
-        self.best_pars = best
-
-        if self.verbose: print('Making results structure...')
-        results = []
-        n_trials = len(study.trials)
-        failed_trials = []
-        for trial in study.trials:
-            data = {'index':trial.number, 'mismatch': trial.value}
-            for key,val in trial.params.items():
-                data[key] = val
-            if data['mismatch'] is None:
-                failed_trials.append(data['index'])
-            else:
-                results.append(data)
-        if self.verbose: print(f'Processed {n_trials} trials; {len(failed_trials)} failed')
-
-        keys = ['index', 'mismatch'] + list(best.keys())
-        data = sc.objdict().make(keys=keys, vals=[])
-        for i,r in enumerate(results):
-            for key in keys:
-                if key not in r:
-                    warnmsg = f'Key {key} is missing from trial {i}, replacing with default'
-                    print(warnmsg)
-                    r[key] = best[key]
-                data[key].append(r[key])
-        self.study_data = data
-        df = sc.dataframe.from_dict(data)
-        df = df.sort_values(by=['mismatch']) # Sort
-
+        df = study.trials_dataframe(multi_index=True)
+        df = df.loc[df.pop('state') == 'COMPLETE']
+        df = df[['number','params','value','user_attrs']]
+        df.insert(1,'seeds', df.pop('user_attrs')['seeds'])
+        df = df.rename({'number':'trial','value':'mismatch'}, axis=1)
+        df = df.set_index('trial')
+        df = df.sort_values('mismatch',ascending=False)
         return df
 
     def to_json(self, filename=None, indent=2, **kwargs):
         """ Convert the results to JSON """
-        order = np.argsort(self.df['mismatch'])
         json = []
-        for o in order:
-            row = self.df.iloc[o,:].to_dict()
-            rowdict = dict(index=row.pop('index'), mismatch=row.pop('mismatch'), pars={})
-            for key,val in row.items():
-                rowdict['pars'][key] = val
-            json.append(rowdict)
-        self.json = json
+        df = self.df.copy()
+        df.columns = self.df.columns.droplevel(1)
+        df = df.loc[:, df.columns != 'params']
+        for trial, params in zip(df.reset_index().to_dict(orient='records'), self.df['params'].to_dict(orient='records')):
+            trial['pars'] = params
+            json.append(trial)
+
         if filename:
             return sc.savejson(filename, json, indent=indent, **kwargs)
         else:
