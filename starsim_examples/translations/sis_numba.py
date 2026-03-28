a687a9e0c50963fb6137091440e480c79b112207
Cliff Kerr <questionably@gmail.com>
2026-03-28 10:34:52 -0400

    add jax version

--- starsim_examples/translations/sis_numba.py
+++ starsim_examples/translations/sis_numba.py
@@ -19,11 +19,8 @@ def boolzeros(*args, **kwargs):
 
 #%% Numba-optimized kernels
 
-jit_kw = dict(
-    cache    = True, 
-    fastmath = True, 
-)
-parallel = True
+# NB: fastmath and parallel make little difference
+jit_kw = dict(cache=True, fastmath=False, parallel=False)
 
 @nb.njit(**jit_kw)
 def nb_get_edges(inds, n_contacts):
@@ -50,14 +47,14 @@ def nb_get_edges(inds, n_contacts):
     return source, target
 
 
-@nb.njit(**jit_kw, parallel=parallel)
+@nb.njit(**jit_kw)
 def nb_end_pairs(p1, p2, edge_beta, edge_dur):
     """ Decrement durations and compact active edges in fused passes """
     n = len(p1)
 
     # First pass: decrement and count active
     n_active = 0
-    for i in nb.prange(n):
+    for i in range(n):
         edge_dur[i] -= 1
         if edge_dur[i] > 0:
             n_active += 1
@@ -79,17 +76,17 @@ def nb_end_pairs(p1, p2, edge_beta, edge_dur):
     return new_p1, new_p2, new_beta, new_dur
 
 
-@nb.njit(**jit_kw, parallel=parallel)
+@nb.njit(**jit_kw)
 def nb_step_state(infected, susceptible, ti_recovered, ti):
     """ Progress infectious -> recovered in one fused pass """
-    for i in nb.prange(len(infected)):
+    for i in range(len(infected)):
         if infected[i] and ti_recovered[i] <= ti:
             infected[i] = False
             susceptible[i] = True
 
 
 @nb.njit(**jit_kw)
-def nb_update_immunity(immunity, rel_sus, waning, parallel=parallel):
+def nb_update_immunity(immunity, rel_sus, waning):
     """ Wane immunity and update relative susceptibility in one pass; return mean rel_sus """
     total = 0.0
     n = len(immunity)
@@ -105,7 +102,7 @@ def nb_update_immunity(immunity, rel_sus, waning, parallel=parallel):
     return total / n
 
 
-@nb.njit(**jit_kw, parallel=parallel)
+@nb.njit(**jit_kw)
 def nb_infect(p1, p2, edge_beta, infected, susceptible, rel_sus, disease_beta, n_agents):
     """ Fused edge traversal, transmission sampling, and deduplication """
     n_edges = len(p1)
@@ -113,7 +110,7 @@ def nb_infect(p1, p2, edge_beta, infected, susceptible, rel_sus, disease_beta, n
     # Boolean flag array for O(1) deduplication
     is_target = np.zeros(n_agents, dtype=np.bool_)
 
-    for i in nb.prange(n_edges):
+    for i in range(n_edges):
         a = p1[i]
         b = p2[i]
         eb = edge_beta[i]
@@ -152,7 +149,7 @@ def nb_set_prognoses(uids, susceptible, infected, immunity, ti_recovered, imm_bo
     sigma = np.sqrt(np.log(2.0))  # Simplifies when std == mean
     mu = np.log(dur_inf_mean) - sigma * sigma / 2.0
 
-    for i in nb.prange(len(uids)):
+    for i in range(len(uids)):
         uid = uids[i]
         susceptible[uid] = False
         infected[uid] = True
