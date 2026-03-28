8b43e4e914575fef5decbfe46a8c13b70980c716
Cliff Kerr <questionably@gmail.com>
2026-03-28 06:40:53 -0400

    add translations folder

---
+++ starsim_examples/translations/sis_starsim.py
@@ -0,0 +1,23 @@
+"""
+Test simulation performance
+"""
+import starsim as ss
+import sciris as sc
+
+# Define the parameters
+n_agents = 100_000
+dur = 100
+
+# Create the sim
+sim = ss.Sim(
+    n_agents=n_agents,
+    dur=dur,
+    diseases = 'sis',
+    networks = 'random',
+    verbose = 0,
+)
+
+# Run and time
+T = sc.timer()
+sim.run()
+T.toc(f'Time for SIS-Starsim, n_agents={n_agents}, dur={dur}')
