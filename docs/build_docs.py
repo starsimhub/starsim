de737e82200e22772077161f2afacc16e578e267
Cliff Kerr <questionably@gmail.com>
2026-04-02 23:49:11 -0400

    switch build_docs.py over to quarto version

--- docs/build_docs.py
+++ docs/build_docs.py
@@ -3,12 +3,8 @@
 Build documentation with Quarto; usage is simply:
 ./build_docs.py
 
-Notebook execution options are:
-  ./build_docs.py jupyter # Execute with jcache in parallel (default)
-  ./build_docs.py quarto # Execute with quarto in serial
-
 Additional arguments are passed to quarto render, e.g.:
-    ./build_docs.py quarto --cache-refresh
+    ./build_docs.py --cache-refresh
 
 To do a full build as for publishing, use (alias to the above):
     ./build_docs.py full
@@ -30,40 +26,26 @@ def run(cmd):
 
 debug = False
 execute = None
-folders = ['tutorials', 'user_guide']
-valid = ['jupyter', 'quarto', 'none']
 args = list(sys.argv)
 argstr = ''
+
 if 'notidy' in args:
     args.remove('notidy')
     tidy = False
 else:
     tidy = True
+
 if 'full' in args:
     args.remove('full')
-    execute = 'quarto'
     argstr = '--cache-refresh'
-
-if len(args) > 1:
-    execute = args[1]
-    if execute == '--execute': execute = 'quarto' # Replace as an alias
-    if execute not in valid:
-        errormsg = f'{execute} not a valid choice; choices are {sc.strjoin(valid)}'
-        raise ValueError(errormsg)
-    argstr = sc.strjoin(args[2:], sep=' ')
-elif execute is None:
-    execute = 'jupyter'
-
-if execute == 'jupyter' and not all([os.path.exists(f'./{folder}/.jupyter_cache') for folder in folders]):
-    print('Falling back to quarto docs build since Jupyter cache not yet initialized')
-    execute = 'quarto'
+else:
+    argstr = '--use-freezer'
 
 
-
-sc.heading(f'Building docs with Quarto and execute="{execute}"', divider='★')
+sc.heading(f'Starting docs build with args={argstr}', divider='★')
 T = sc.timer()
 
-sc.heading('Updating docs version number...')
+sc.heading('1/6 Updating docs version number...')
 filename = '_variables.yml'
 data = dict(version=ss.__version__, versiondate=ss.__versiondate__)
 orig = sc.loadyaml(filename)
@@ -72,38 +54,33 @@ if data != orig:
     print('Version updated to:', data)
 else:
     print('Version already correct:', orig)
+T.toctic()
 
 
-# Generate API documentation using quartodoc
-sc.heading('Building API documentation...')
+# Build the docs
+sc.heading('2/6 Building API documentation...')
 run('python -m quartodoc build')
+T.toctic()
 
-sc.heading('Customizing API aliases...')
+sc.heading('3/6 Customizing API aliases...')
 ut.customize_aliases()
+T.toctic()
 
-sc.heading('Building docs links...')
+sc.heading('4/6 Building docs links...')
 run('python -m quartodoc interlinks')
+T.toctic()
 
-if execute == 'jupyter':
-    sc.heading('Executing Jupyter notebooks via jcache')
-    run('./jcache_execute_notebooks')
+sc.heading('5/6 Rendering docs with notebook execution...')
+run(f'quarto render --execute {argstr}')
+T.toctic()
 
-# Build the Quarto documentation
-if execute == 'quarto':
-    sc.heading('Rendering docs with notebook execution...')
-    run(f'quarto render --execute {argstr}')
-else:
-    sc.heading('Rendering docs...')
-    run(f'quarto render{argstr}')
 
 # Tidy up
 if tidy:
-    sc.heading("Normalizing notebooks...")
-    run('./normalize_notebooks.py')
-
-    sc.heading("Tidying outputs...")
+    sc.heading("6/6 Tidying outputs...")
     run('./clean_outputs.py')
+    T.toctic()
 
 
-T.toc('Docs built')
-print(f"\nIndex:\n{os.getcwd()}/_site/index.html")
+sc.printyellow(f'\nDone: docs built in {T.total:0.0f} s')
+sc.printcyan(f"\nIndex:\n{os.getcwd()}/_site/index.html")
