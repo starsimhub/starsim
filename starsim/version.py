327211b51485f84251214c48523ab93139636103
Cliff Kerr <questionably@gmail.com>
2026-08-28 00:38:08 -0400

    update version date

--- starsim/version.py
+++ starsim/version.py
@@ -3,5 +3,5 @@ Version and license information.
 """
 
 __version__ = '3.6.0'
-__versiondate__ = '2026-08-26'
+__versiondate__ = '2026-08-28'
 __license__ = f'Starsim {__version__} ({__versiondate__}) — © 2023-2026 by the Gates Foundation / Starsim Development Team'
